6c355c7431fa45a3550c679b90c3f589409e8f9e
topazape <topazape@gmail.com>
2021-07-28 23:18:15 +0900

    add many

--- rc/config/wezterm/wezterm.lua
+++ rc/config/wezterm/wezterm.lua
@@ -1,28 +1,83 @@
-local wezterm = require('wezterm');
+local wezterm = require('wezterm')
+
+wezterm.on('format-tab-title',
+  function(tab)
+    local title = tab.active_pane.title
+    local width = wezterm.column_width(title)
+    return {
+      {Background={Color='#c0c0c0'}},
+      {Foreground={Color='#2b2042'}},
+      {Text=' ' .. title .. ' '},
+      {Background={Color='#2b2042'}},
+      {Foreground={Color='#c0c0c0'}},
+      {Text=utf8.char(0xe0b0)},
+    }
+  end
+)
 
 local config = {
   -- fonts
   font = wezterm.font_with_fallback({
-    'JetBrains Mono',
-    'Hack Nerd Font Mono',
-    'Noto Sans CJK JP'
+    {family='JetBrainsMono Nerd Font', weight='Regular'},
   }),
+  font_rules = {
+    {
+      intensity = 'Normal',
+      italic = false,
+      font = wezterm.font_with_fallback(
+        {
+          {family='JetBrainsMono Nerd Font', weight='Regular', italic=false},
+        }
+      ),
+    },
+    {
+      intensity = 'Normal',
+      italic = true,
+      font = wezterm.font_with_fallback(
+        {
+          {family='JetBrainsMono Nerd Font', weight='Regular', italic=true},
+        }
+      ),
+    },
+    {
+      intensity = 'Bold',
+      italic = false,
+      font = wezterm.font_with_fallback(
+        {
+          {family='JetBrainsMono Nerd Font', weight='Bold', italic=false},
+        }
+      ),
+    },
+    {
+      intensity = 'Bold',
+      italic = true,
+      font = wezterm.font_with_fallback(
+        {
+          {family='JetBrainsMono Nerd Font', weight='Bold', italic=true},
+        }
+      ),
+    },
+  },
   dpi = 144.0,
   font_size = 14.0,
-  font_shaper = 'Harfbuzz',
-  harfbuzz_features = {'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'zero', 'onum'},
 
-  --colors
+  -- colors
   color_scheme_dirs = {
-    os.getenv('HOME')..'/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/extras',
+    wezterm.home_dir .. '/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/extras',
   },
   color_scheme = 'wezterm_tokyonight_storm',
 
+  -- tabs
+  tab_bar_at_bottom = true,
+  tab_max_width = 32,
+
+  -- bindings
+  -- disable_default_key_bindings = true,
+  -- disable_default_mouse_bindings = true,
+
   -- misc
   use_ime = true,
-  enable_tab_bar = false,
-  disable_default_key_bindings = true,
-  disable_default_mouse_bindings = true,
+  check_for_updates = false,
 }
 
 return config
