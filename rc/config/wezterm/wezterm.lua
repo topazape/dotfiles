d9d2a20d77eabaf4f3b42f7915fd9236343dc524
topazape <topazape@gmail.com>
2021-09-09 17:20:25 +0900

    yes

--- rc/config/wezterm/wezterm.lua
+++ rc/config/wezterm/wezterm.lua
@@ -16,15 +16,18 @@ local image = images[math.random(#images)]
 return {
   -- fonts
   font = wezterm.font_with_fallback({
-    {family='JetBrains Mono', weight='Regular'},
-    {family='Toppan Bunkyu Gothic', weight='Regular'},
-    {family='Hack Nerd Font Mono', weight='Regular'}
+    {family='JetBrains Mono', weight='Light'},
+    {family='Apple Symbols', weight='Regular'},
+    {family='Apple Color Emoji', weight='Regular'},
+
+    {family='FiraCode Nerd Font', weight='Light'},
+    {family='JuliaMono', weight='Light'},
+    {family='Tsukushi A Round Gothic', weight='Regular'},
   }),
   font_shaper = 'Harfbuzz',
   harfbuzz_features = {'zero'},
-  -- font_shaper = 'Allsorts',
-  dpi = 96.0,
-  font_size = 14,
+  dpi = 144.0,
+  font_size = 14.0,
 
   -- colors
   color_scheme_dirs = {
@@ -32,19 +35,19 @@ return {
   },
   color_scheme = 'wezterm_tokyonight_storm',
 
+  -- window
+  window_padding = { left = 1, right = 1, bottom = 1 },
   -- backgrounds
-  text_background_opacity = 0.9,
+  text_background_opacity = 0.8,
   window_background_image = image,
   window_background_image_hsb = {
-    brightness = 0.3,
+    brightness = 0.08,
     hue = 1.0,
     saturation = 1.0,
   },
 
   -- tabs
   enable_tab_bar =false,
-  -- tab_bar_at_bottom = true,
-  -- tab_max_width = 32,
 
   -- bindings
   disable_default_key_bindings = true,
@@ -54,12 +57,11 @@ return {
     {key='f', mods='SUPER', action='ToggleFullScreen'},
     {key='r', mods='SUPER', action='ReloadConfiguration'}
   },
-  -- disable_default_mouse_bindings = true,
+  disable_default_mouse_bindings = false,
 
   -- misc
   use_ime = true,
   check_for_updates = false,
-  -- allow_square_glyphs_to_overflow_width = 'WhenFollowedBySpace',
-  allow_square_glyphs_to_overflow_width = 'Always',
+  allow_square_glyphs_to_overflow_width = 'WhenFollowedBySpace',
   native_macos_fullscreen_mode = true,
 }
