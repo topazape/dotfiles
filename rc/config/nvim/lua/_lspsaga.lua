727cad27c8444dfbf796ca37c4dd419629f27da1
topazape <topazape@gmail.com>
2021-09-09 18:02:16 +0900

    natural syntax

--- rc/config/nvim/lua/_lspsaga.lua
+++ rc/config/nvim/lua/_lspsaga.lua
@@ -1,8 +1,9 @@
 local saga = require('lspsaga')
-saga.init_lsp_saga {
+
+saga.init_lsp_saga({
   error_sign = '',
   warn_sign = '',
   hint_sign = '',
   infor_sign = '',
   border_style = 'round',
-}
+})
