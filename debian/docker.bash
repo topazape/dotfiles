87d4b767f88e96ba52e41b39954ac6c7452f2ad6
topazape <topazape@gmail.com>
2021-11-08 17:28:09 +0900

    debian specific scripts

---
+++ debian/docker.bash
@@ -0,0 +1,5 @@
+keyring_name=docker-archive-keyring.gpg codename=$(. /etc/os-release;echo $VERSION_CODENAME) \
+&& curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/$keyring_name \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/$keyring_name] \
+https://download.docker.com/linux/debian $codename stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
