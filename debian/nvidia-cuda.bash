c1299a00d8cca6b4ddfff21051f6d66da1f84ba5
topazape <topazape@gmail.com>
2022-04-19 18:52:46 +0900

    .

--- debian/nvidia-cuda.bash
+++ debian/nvidia-cuda.bash
@@ -2,4 +2,4 @@ keyring_name=nvidia-cuda-archive-keyring.gpg dist=$(. /etc/os-release;echo $ID$V
 	curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/$dist/x86_64/7fa2af80.pub |
 	sudo gpg --dearmor -o /usr/share/keyrings/$keyring_name &&
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/$keyring_name] https://developer.download.nvidia.com/compute/cuda/repos/$dist/x86_64/ /" |
-	sudo tee /etc/apt/sources.list.d/nvidia_cuda.list > /dev/null
+	sudo tee /etc/apt/sources.list.d/nvidia-cuda.list > /dev/null
