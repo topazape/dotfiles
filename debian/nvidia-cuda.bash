5098bd27e5d076a893fd148b176784e6eaa1ac3b
topazape <topazape@gmail.com>
2022-04-29 11:17:20 +0900

    change pub-key name to fix apt-get update

--- debian/nvidia-cuda.bash
+++ debian/nvidia-cuda.bash
@@ -1,5 +1,5 @@
 keyring_name=nvidia-cuda-archive-keyring.gpg dist=$(. /etc/os-release;echo $ID$VERSION_ID) &&
-	curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/$dist/x86_64/7fa2af80.pub |
+	curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/$dist/x86_64/3bf863cc.pub |
 	sudo gpg --dearmor -o /usr/share/keyrings/$keyring_name &&
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/$keyring_name] https://developer.download.nvidia.com/compute/cuda/repos/$dist/x86_64/ /" |
 	sudo tee /etc/apt/sources.list.d/nvidia-cuda.list
