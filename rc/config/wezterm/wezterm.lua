05aed2b13ffca4db46cb103571b7afb58a209e85
topazape <topazape@gmail.com>
2022-05-08 19:31:50 +0900

    change font

--- rc/config/wezterm/wezterm.lua
+++ rc/config/wezterm/wezterm.lua
@@ -2,13 +2,16 @@ local wezterm = require("wezterm")
 
 return {
 	font = wezterm.font_with_fallback({
-		{family="BlexMono Nerd Font Mono", weight="Regular"},
+		{family="JetBrains Mono Nerd Font", weight="Regular"},
+		{family="BlexMono Nerd Font", weight="Regular"},
 		{family="M+ 1m", weight="Regular"}
 	}),
 	font_size = 11.0,
 	color_scheme = "Afterglow",
 	use_ime = true,
 	warn_about_missing_glyphs=false,
+	initial_cols = 160,
+	initial_rows = 48,
 	
 	disable_default_key_bindings = true,
 	keys = {
