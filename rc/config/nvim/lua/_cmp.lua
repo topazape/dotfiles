4a464dd9e41c83a5cd54f702a635f195e9351195
topazape <topazape@gmail.com>
2022-05-25 08:38:13 +0900

    change keymap

--- rc/config/nvim/lua/_cmp.lua
+++ rc/config/nvim/lua/_cmp.lua
@@ -58,13 +58,13 @@ cmp.setup({
   mapping = {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-j>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     -- Accept currently selected item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ['<C-o>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-f>'] = cmp.mapping.confirm({ select = true }),
   },
 
   sources = {
