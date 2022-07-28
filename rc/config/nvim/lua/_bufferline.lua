f69dcd900ebe4479f4ea28c645186626e3c75027
topazape <topazape@gmail.com>
2022-07-28 20:54:08 +0900

    add icon options and keymaps

--- rc/config/nvim/lua/_bufferline.lua
+++ rc/config/nvim/lua/_bufferline.lua
@@ -1,6 +1,20 @@
 local bufferline = require('bufferline')
 
-bufferline.setup({})
+bufferline.setup({
+  options = {
+    color_icons = true,
+    show_buffer_icons = true,
+    show_buffer_close_icons = false,
+    show_buffer_default_icon = true,
+    show_close_icon = false,
+    show_tab_indicators = true,
+    separator_style = {'‖', '|'},
+    enforce_regular_tabs = false,
+    always_show_bufferline = true
+  }
+})
 
 vim.api.nvim_set_keymap('n', '<C-j>', ':BufferLineCycleNext<CR>', {})
 vim.api.nvim_set_keymap('n', '<C-k>', ':BufferLineCyclePrev<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>rh', ':BufferLineCloseLeft<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>rl', ':BufferLineCloseRight<CR>', {})
