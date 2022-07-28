2eef3a678f972cc59b933585c4e6d7acbbfbdf6f
topazape <topazape@gmail.com>
2022-07-28 21:04:43 +0900

    add buffer no and close keymap

--- rc/config/nvim/lua/_bufferline.lua
+++ rc/config/nvim/lua/_bufferline.lua
@@ -2,6 +2,7 @@ local bufferline = require('bufferline')
 
 bufferline.setup({
   options = {
+    numbers = 'ordinal',
     color_icons = true,
     show_buffer_icons = true,
     show_buffer_close_icons = false,
@@ -16,5 +17,14 @@ bufferline.setup({
 
 vim.api.nvim_set_keymap('n', '<C-j>', ':BufferLineCycleNext<CR>', {})
 vim.api.nvim_set_keymap('n', '<C-k>', ':BufferLineCyclePrev<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>rh', ':BufferLineCloseLeft<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>rl', ':BufferLineCloseRight<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>c', ':BufferLinePickClose<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>1', ':BufferLineGoToBuffer 1<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>2', ':BufferLineGoToBuffer 2<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>3', ':BufferLineGoToBuffer 3<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>4', ':BufferLineGoToBuffer 4<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>5', ':BufferLineGoToBuffer 5<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>6', ':BufferLineGoToBuffer 6<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>7', ':BufferLineGoToBuffer 7<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>8', ':BufferLineGoToBuffer 8<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>9', ':BufferLineGoToBuffer 9<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>$', ':BufferLineGoToBuffer -1<CR>', {})
