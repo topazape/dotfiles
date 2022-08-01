f15b361c868fdbeb3d914b06268430372acf8e62
topazape <topazape@gmail.com>
2022-08-01 09:48:14 +0900

    change align

--- rc/config/nvim/lua/_dap.lua
+++ rc/config/nvim/lua/_dap.lua
@@ -110,13 +110,9 @@ dapui.setup({
   }
 })
 
--- key config like VS-Code
+
+-- basic config
 vim.api.nvim_set_keymap('n', '<leader>d', ':lua require("dapui").toggle()<CR>', {})
-vim.api.nvim_set_keymap('n', '<F5>', ':DapContinue<CR>', {})
-vim.api.nvim_set_keymap('n', '<F6>', ':DapTerminate<CR>', {})
-vim.api.nvim_set_keymap('n', '<F10>', ':DapStepOver<CR>', {})
-vim.api.nvim_set_keymap('n', '<F11>', ':DapStepInto<CR>', {})
-vim.api.nvim_set_keymap('n', '<F12>', ':DapStepOut<CR>', {})
 
 vim.api.nvim_set_keymap('n', '<leader>k', ':lua require("dapui").eval()<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>b', ':DapToggleBreakpoint<CR>', {})
@@ -124,3 +120,10 @@ vim.api.nvim_set_keymap('n', '<leader>B', ':lua require("dap").set_breakpoint(ni
 vim.api.nvim_set_keymap('n', '<leader>lp', ':lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>dr', ':lua require("dap").repl.open()<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>dl', ':lua require("dap").run_last()<CR>', {})
+
+-- key config like VS-Code
+vim.api.nvim_set_keymap('n', '<F5>', ':DapContinue<CR>', {})
+vim.api.nvim_set_keymap('n', '<F6>', ':DapTerminate<CR>', {})
+vim.api.nvim_set_keymap('n', '<F10>', ':DapStepOver<CR>', {})
+vim.api.nvim_set_keymap('n', '<F11>', ':DapStepInto<CR>', {})
+vim.api.nvim_set_keymap('n', '<F12>', ':DapStepOut<CR>', {})
