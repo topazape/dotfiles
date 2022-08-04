111a470470658b0758b09de5c55425abcad3d1a5
topazape <topazape@gmail.com>
2022-08-04 21:58:49 +0900

    enable lsp

--- rc/config/nvim/lua/_mason.lua
+++ rc/config/nvim/lua/_mason.lua
@@ -1,4 +1,10 @@
 local mason = require('mason')
-local mason_lspconfig = require('mason-lspconfig')
 mason.setup({})
+
+local mason_lspconfig = require('mason-lspconfig')
 mason_lspconfig.setup({})
+mason_lspconfig.setup_handlers({
+  function(server_name)
+    require('lspconfig')[server_name].setup({})
+  end
+})
