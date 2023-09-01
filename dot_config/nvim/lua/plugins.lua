9d5a173780f42d06811a214bfaa78a9a441e06e6
topazape <topazape@gmail.com>
2023-09-01 12:18:08 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -42,7 +42,7 @@ return {
 		"kosayoda/nvim-lightbulb",
 		config = function()
 			require("nvim-lightbulb").setup({
-				sign = { enabled = true, text = "☪" },
+				sign = { enabled = true, text = "☪", hl = "LightBulbSign" },
 				virtual_text = { enabled = false },
 				float = { enabled = false },
 				status_text = { enabled = false },
