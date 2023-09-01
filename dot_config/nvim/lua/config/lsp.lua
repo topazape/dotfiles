1e353b59955b82580515d9bc3a73c38269a577e3
topazape <topazape@gmail.com>
2023-09-01 12:43:06 +0900

    Update .config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -1,14 +1,6 @@
 local navic = require("nvim-navic")
 
 local on_attach = function(client, bufnr)
-	--
-	vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-		virtual_text = {
-			format = function(diagnostic)
-				return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
-			end,
-		},
-	})
 	-- navic
 	if client.server_capabilities.documentSymbolProvider then
 		navic.attach(client, bufnr)
