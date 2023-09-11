85b9658bc72568c65ce59b30d82a0654d1c1515f
topazape <topazape@gmail.com>
2023-09-11 14:05:53 +0900

    Update .config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -85,6 +85,19 @@ mason_lspconfig.setup_handlers({
 			default_config = {
 				cmd = { "golangci-lint-langserver" },
 				root_dir = require("lspconfig").util.root_pattern(".git", "go.mod"),
+				init_options = {
+					command = {
+						"golangci-lint",
+						"run",
+						"--enable",
+						"gofumpt",
+						"--enable",
+						"goimports",
+						"--out-format",
+						"json",
+						"--issues-exit-code=1",
+					},
+				},
 			},
 		})
 	end,
