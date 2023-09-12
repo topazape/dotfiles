818086c7deab974586175b4a9baed76ef38c94d8
topazape <topazape@gmail.com>
2023-09-12 13:40:39 +0900

    Update .config/zsh/local.zsh

--- dot_config/zsh/local.zsh
+++ dot_config/zsh/local.zsh
@@ -87,7 +87,7 @@ if type zellij > /dev/null 2>&1; then
 fi
 ## bat
 if type bat > /dev/null 2>&1; then
-  export BAT_THEME="OneHalfDark"
+  export BAT_THEME="ansi"
 fi
 ## vivid
 if type vivid > /dev/null 2>&1; then
