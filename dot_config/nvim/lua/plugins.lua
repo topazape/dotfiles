b2ec93ac3dfba39074adefccea19990a3609f7a0
topazape <topazape@gmail.com>
2023-09-20 22:26:42 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -44,9 +44,11 @@ return {
 			require("lspsaga").setup({
 				-- keymaps for lspsaga
 				vim.keymap.set("n", "ga", "<CMD>Lspsaga code_action<CR>", { noremap = true }),
-				vim.keymap.set("n", "ol", "<CMD>Lspsaga outline<CR>", { noremap = true }),
+				vim.keymap.set("n", "go", "<CMD>Lspsaga outline<CR>", { noremap = true }),
 				-- ui
-				ui = { border = "double" },
+				ui = {
+					border = "double",
+				},
 			})
 		end,
 		dependencies = {
