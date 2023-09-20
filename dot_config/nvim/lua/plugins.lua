91c313362332f09d7bb30f93860f6f985ef3d2ce
topazape <topazape@gmail.com>
2023-09-20 22:16:53 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -46,6 +46,7 @@ return {
 				vim.keymap.set("n", "ga", "<CMD>Lspsaga code_action<CR>", { noremap = true }),
 				-- ui
 				ui = {
+					title = false,
 					lines = { "┗", "┣", "┃", "━", "┏" },
 				},
 			})
