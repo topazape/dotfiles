b579b46f98ef28be755aa66b3c97f12b1c9cf716
topazape <topazape@gmail.com>
2023-09-26 13:58:08 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -41,37 +41,7 @@ return {
 	{
 		"nvimdev/lspsaga.nvim",
 		config = function()
-			require("lspsaga").setup({
-				-- keymaps for lspsaga
-				vim.keymap.set("n", "K", "<CMD>Lspsaga hover_doc<CR>", { noremap = true }),
-				vim.keymap.set("n", "gd", "<CMD>Lspsaga peek_definition<CR>", { noremap = true }),
-				vim.keymap.set("n", "gr", "<CMD>Lspsaga finder<CR>", { noremap = true }),
-				vim.keymap.set("n", "ga", "<CMD>Lspsaga code_action<CR>", { noremap = true }),
-				vim.keymap.set("n", "gn", "<CMD>Lspsaga rename<CR>", { noremap = true }),
-				vim.keymap.set("n", "ge", "<CMD>Lspsaga show_line_diagnostics<CR>", { noremap = true }),
-				vim.keymap.set("n", "go", "<CMD>Lspsaga outline<CR>", { noremap = true }),
-				vim.keymap.set("n", "g]", "<CMD>Lspsaga diagnostic_jump_next<CR>", { noremap = true }),
-				vim.keymap.set("n", "g[", "<CMD>Lspsaga diagnostic_jump_prev<CR>", { noremap = true }),
-				vim.keymap.set("n", "<A-t>", "<CMD>Lspsaga term_toggle<CR>", { noremap = true }),
-				-- definition
-				definition = {
-					width = 0.7,
-					height = 0.6,
-					keys = {
-						close = "k",
-						edit = "e",
-						quit = "q",
-						split = "s",
-						vsplit = "v",
-					},
-				},
-				-- ui
-				ui = {
-					border = "rounded", -- Can be single, double, rounded, solid, shadow.
-				},
-				scroll_down = "<C-n>",
-				scroll_up = "<C-p>",
-			})
+			require("config/lspsaga")
 		end,
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
