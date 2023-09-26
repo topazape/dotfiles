c5a5f3699209bdedc0e20306b137013c6ecedeb7
topazape <topazape@gmail.com>
2023-09-26 23:34:51 +0900

    Update .config/nvim/lua/config/lspsaga.lua

--- dot_config/nvim/lua/config/lspsaga.lua
+++ dot_config/nvim/lua/config/lspsaga.lua
@@ -26,7 +26,7 @@ lspsaga.setup({
 	-- ui
 	ui = {
 		border = "rounded", -- Can be single, double, rounded, solid, shadow.
-		kind = require("lspsaga.lspkind").kind,
+		kind = lspsaga.get_kind(),
 	},
 	scroll_down = "<C-n>",
 	scroll_up = "<C-p>",
