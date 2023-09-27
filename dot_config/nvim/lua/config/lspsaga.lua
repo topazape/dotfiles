d71c08027a963d2a145a2d657621e5c8f426f43e
topazape <topazape@gmail.com>
2023-09-27 09:37:44 +0900

    Update .config/nvim/lua/config/lspsaga.lua

--- dot_config/nvim/lua/config/lspsaga.lua
+++ dot_config/nvim/lua/config/lspsaga.lua
@@ -30,7 +30,7 @@ lspsaga.setup({
 	},
 	ui = {
 		border = "rounded", -- Can be single, double, rounded, solid, shadow.
-		lines = { "L", "ト", "|", "-", "「" },
+		lines = { "└", "├", "│", "─", "┌" },
 	},
 	scroll_down = "<C-n>",
 	scroll_up = "<C-p>",
