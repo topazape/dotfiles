b402da189a0d36687b48b02399561145f5aecd82
topazape <topazape@gmail.com>
2023-10-04 22:37:50 +0900

    Update .config/zsh/local.zsh

--- dot_config/zsh/local.zsh
+++ dot_config/zsh/local.zsh
@@ -155,7 +155,7 @@ fi
 ### rustup
 if type rustup-init > /dev/null 2>&1; then
   export RUSTUP_HOME=${XDG_DATA_HOME}/rustup
-
+  export CARGO_HOME=${XDG_DATA_HOME}/cargo
 fi
 ### cargo
 if type cargo > /dev/null 2>&1; then
