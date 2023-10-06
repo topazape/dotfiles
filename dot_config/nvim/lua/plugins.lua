cf405f96e664ecd27a313be51bfba9032410e837
topazape <topazape@gmail.com>
2023-10-06 14:07:36 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -75,41 +75,41 @@ return {
 		end,
 	},
 	---- copilot
-	-- {
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	event = "VeryLazy",
-	-- 	config = function()
-	-- 		require("copilot").setup({
-	-- 			panel = {
-	-- 				enabled = false,
-	-- 			},
-	-- 			suggestion = {
-	-- 				enabled = true,
-	-- 				auto_trigger = true,
-	-- 				debounce = 100,
-	-- 				keymap = {
-	-- 					accept = "<TAB>",
-	-- 					accept_word = false,
-	-- 					accept_line = false,
-	-- 					next = "<M-]>",
-	-- 					prev = "<M-[>",
-	-- 					dismiss = "<C-]>",
-	-- 				},
-	-- 			},
-	-- 			filetypes = {
-	-- 				cvs = false,
-	-- 				gitcommit = false,
-	-- 				gitrebase = false,
-	-- 				help = false,
-	-- 				hgcommit = false,
-	-- 				markdown = true,
-	-- 				svn = false,
-	-- 				yaml = true,
-	-- 				["."] = false,
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"zbirenbaum/copilot.lua",
+		event = "VeryLazy",
+		config = function()
+			require("copilot").setup({
+				panel = {
+					enabled = false,
+				},
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					debounce = 100,
+					keymap = {
+						accept = "<TAB>",
+						accept_word = false,
+						accept_line = false,
+						next = "<M-]>",
+						prev = "<M-[>",
+						dismiss = "<C-]>",
+					},
+				},
+				filetypes = {
+					cvs = false,
+					gitcommit = false,
+					gitrebase = false,
+					help = false,
+					hgcommit = false,
+					markdown = true,
+					svn = false,
+					yaml = true,
+					["."] = false,
+				},
+			})
+		end,
+	},
 
 	-- dap
 	{
