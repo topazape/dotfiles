390d21be7467a4bd2db1f4a2cae83119b7a4bb4d
topazape <topazape@gmail.com>
2023-10-25 11:36:27 +0900

    Update .config/nvim/lua/config/format-on-save.lua

--- dot_config/nvim/lua/config/format-on-save.lua
+++ dot_config/nvim/lua/config/format-on-save.lua
@@ -17,7 +17,7 @@ format_on_save.setup({
 			formatters.stylua, -- default
 		},
 		python = {
-			formatters.black, -- default
+			formatters.shell({ cmd = { "ruff", "format", "--no-cache", "--respect-gitignore" } }),
 		},
 		rust = {
 			formatters.shell({ cmd = { "rustfmt", "--emit=stdout" } }),
