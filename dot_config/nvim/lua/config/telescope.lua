aaa3d5c82d933f1bab5ad017d446ab5bbae2c810
topazape <topazape@gmail.com>
2023-10-25 21:53:16 +0900

    Update .config/nvim/lua/config/telescope.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -39,7 +39,7 @@ telescope.setup({
 	pickers = {
 		find_files = {
 			hidden = true,
-			file_ignore_patterns = { "^Library/" },
+			file_ignore_patterns = { "^Library/", "^.cache/" },
 		},
 	},
 	extensions = {
