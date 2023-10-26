40d330bd6007e72c40050dd906f4e5d096953fff
topazape <topazape@gmail.com>
2023-10-26 22:45:14 +0900

    Update .config/nvim/lua/config/lualine.lua

--- dot_config/nvim/lua/config/lualine.lua
+++ dot_config/nvim/lua/config/lualine.lua
@@ -11,7 +11,7 @@ end
 lualine.setup({
 	options = {
 		theme = "catppuccin",
-		section_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		component_separators = { left = "", right = "" },
 		globalstatus = true,
 	},
