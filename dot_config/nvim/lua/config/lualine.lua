281bd1a401503627cd148a619d486b2364caa366
topazape <topazape@gmail.com>
2023-10-26 22:50:25 +0900

    Update .config/nvim/lua/config/lualine.lua

--- dot_config/nvim/lua/config/lualine.lua
+++ dot_config/nvim/lua/config/lualine.lua
@@ -39,13 +39,10 @@ lualine.setup({
 		},
 	},
 	tabline = {
-		lualine_b = {
+		lualine_a = {
 			{ "filename", path = 0 },
 			{ "diagnostics" },
 		},
-		-- lualine_c = {
-		-- 	{ "navic", color_correction = "dynamic", navic_opts = nil },
-		-- },
 		lualine_x = {
 			{ lsp_names },
 		},
