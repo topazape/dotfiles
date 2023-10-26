a9240c3e67ad4bfa0e06216442608c492fc34b7c
topazape <topazape@gmail.com>
2023-10-26 09:37:20 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -224,7 +224,7 @@ return {
 
 	{
 		"0x00-ketsu/markdown-preview.nvim",
-		ft = { "md" }, --, "markdown", "mkd", "mkdn", "mdwn", "mdown", "mdtxt", "mdtext", "rmd", "wiki" },
+		ft = { "md", "markdown", "mkd", "mkdn", "mdwn", "mdown", "mdtxt", "mdtext", "rmd", "wiki" },
 		config = function()
 			require("markdown-preview").setup()
 		end,
