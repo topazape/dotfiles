0d00aa64da3d29681f52afff647b94216e6bae1a
topazape <topazape@gmail.com>
2023-10-26 10:58:49 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -232,6 +232,10 @@ return {
 	{
 		dir = "~/Develop/local/markdown-preview.nvim",
 		dev = true,
+		ft = { "md", "markdown", "mkd", "mkdn", "mdwn", "mdown", "mdtxt", "mdtext", "rmd", "wiki" },
+		config = function()
+			require("markdown-preview").setup()
+		end,
 	},
 
 	-- startup
