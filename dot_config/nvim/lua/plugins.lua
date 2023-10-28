19a43b2b47658429439929f12a175de75c6bdc0e
topazape <topazape@gmail.com>
2023-10-28 10:59:43 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -143,7 +143,7 @@ return {
 					-- smart_indent_cap = true,
 				},
 				scope = {
-					enabled = true,
+					enabled = false,
 					show_start = false,
 					show_end = false,
 					injected_languages = false,
