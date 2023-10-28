a9f694ad8e82696b7c564fa12203aea08241aef8
topazape <topazape@gmail.com>
2023-10-28 10:44:33 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -137,17 +137,7 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
-			local highlight = {
-				"CursorColumn",
-				"Whitespace",
-			}
-			require("ibl").setup({
-				indent = { highlight = highlight, char = "" },
-				whitespace = {
-					highlight = highlight,
-					remove_blankline_trail = false,
-				},
-			})
+			require("ibl").setup()
 		end,
 	},
 
