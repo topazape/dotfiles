ca4ff929fdf9578d5730f514d6337781d36c7e79
topazape <topazape@gmail.com>
2023-10-28 11:07:42 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -139,8 +139,8 @@ return {
 		config = function()
 			require("ibl").setup({
 				indent = {
-					char = "▏",
-					tab_char = "▏",
+					char = "╎",
+					tab_char = "╎",
 					smart_indent_cap = true,
 				},
 				scope = {
