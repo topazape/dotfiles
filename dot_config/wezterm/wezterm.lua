e45b2894461a21b9b0a370e7694e78333b83ea48
topazape <topazape@gmail.com>
2023-11-11 09:27:30 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require("wezterm")
 
 return {
 	font = wezterm.font_with_fallback({
-		{ family = "Monaspace Argon" },
 		{ family = "Berkeley Mono" },
 		{ family = "Pretendard JP" },
 	}),
