ccdfb0a6c3b60cdfe666d9ae7e8e895fe1722853
topazape <topazape@gmail.com>
2023-11-21 09:42:14 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -242,7 +242,35 @@ return {
 		end,
 	},
 
-	-- markdown
+	-- org mode
+	{
+		"nvim-orgmode/orgmode",
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter", lazy = true },
+		},
+		envent = "VeryLazy",
+		config = function()
+			-- Load treesitter grammar for org
+			require("orgmode").setup_ts_grammar()
+
+			-- Setup treesitter
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = { "org" },
+				},
+				ensure_installed = { "org" },
+			})
+
+			-- Setup orgmode
+			require("orgmode").setup({
+				org_agenda_files = "~/orgfiles/**/*",
+				org_default_notes_file = "~/orgfiles/refile.org",
+			})
+		end,
+	},
+
+	-- markdown previewer
 	{
 		"topazape/md-preview.nvim",
 		ft = { "md", "markdown", "mkd", "mkdn", "mdwn", "mdown", "mdtxt", "mdtext", "rmd", "wiki" },
