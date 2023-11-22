a9b4148238c612276bac48003249438bf4cf5135
topazape <topazape@gmail.com>
2023-11-22 16:33:19 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -39,7 +39,7 @@ return {
 	send_composed_key_when_left_alt_is_pressed = false,
 	send_composed_key_when_right_alt_is_pressed = false,
 
-	keyconfig,
+	keys = keyconfig,
 
 	color_scheme = "Catppuccin Mocha",
 }
