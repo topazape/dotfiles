3f84578a193d82ea2e57e9c81c05a2dab4ef7a7f
topazape <topazape@gmail.com>
2023-11-22 21:39:59 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -25,7 +25,34 @@ return {
 
 	-- keybindings
 	leader = { mods = "CTRL", key = "t", timeout_milliseconds = 1000 },
-	keys = {},
+	-- keys = {
+	-- 	-- pane
+	-- 	{ key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	-- 	{ key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	-- 	-- tab selection
+	-- 	{ key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
+	-- 	{ key = "F1", mods = "NONE", action = wezterm.action.ActivateTab(0) },
+	-- 	{ key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
+	-- 	{ key = "F2", mods = "NONE", action = wezterm.action.ActivateTab(1) },
+	-- 	{ key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
+	-- 	{ key = "F3", mods = "NONE", action = wezterm.action.ActivateTab(2) },
+	-- 	{ key = "4", mods = "LEADER", action = wezterm.action.ActivateTab(3) },
+	-- 	{ key = "F4", mods = "NONE", action = wezterm.action.ActivateTab(3) },
+	-- 	{ key = "5", mods = "LEADER", action = wezterm.action.ActivateTab(4) },
+	-- 	{ key = "F5", mods = "NONE", action = wezterm.action.ActivateTab(4) },
+	-- 	{ key = "6", mods = "LEADER", action = wezterm.action.ActivateTab(5) },
+	-- 	{ key = "F6", mods = "NONE", action = wezterm.action.ActivateTab(5) },
+	-- 	{ key = "7", mods = "LEADER", action = wezterm.action.ActivateTab(6) },
+	-- 	{ key = "F7", mods = "NONE", action = wezterm.action.ActivateTab(6) },
+	-- 	{ key = "8", mods = "LEADER", action = wezterm.action.ActivateTab(7) },
+	-- 	{ key = "F8", mods = "NONE", action = wezterm.action.ActivateTab(7) },
+	-- 	{ key = "9", mods = "LEADER", action = wezterm.action.ActivateTab(8) },
+	-- 	{ key = "F9", mods = "NONE", action = wezterm.action.ActivateTab(8) },
+	-- 	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+	-- 	{ key = "F11", mods = "NONE", action = wezterm.action.ActivateTabRelative(-1) },
+	-- 	{ key = "]", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+	-- 	{ key = "F12", mods = "NONE", action = wezterm.action.ActivateTabRelative(1) },
+	-- },
 
 	color_scheme = "Catppuccin Mocha",
 }
