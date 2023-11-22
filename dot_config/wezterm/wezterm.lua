27df937adafabc03f2cf419333b39a2c9d08d1d4
topazape <topazape@gmail.com>
2023-11-22 21:14:51 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,31 +1,5 @@
 local wezterm = require("wezterm")
 
-local keyconfig = {
-	{ key = "Copy", mods = "NONE", action = wezterm.action.CopyTo("Clipboard") },
-	{ key = "Paste", mods = "NONE", action = wezterm.action.PasteFrom("Clipboard") },
-	{ mods = "LEADER", key = "n", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-	{ mods = "LEADER", key = "|", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ mods = "LEADER", key = "-", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ mods = "LEADER", key = "1", action = wezterm.action.ActivateTab(0) },
-	{ mods = "LEADER", key = "2", action = wezterm.action.ActivateTab(1) },
-	{ mods = "LEADER", key = "3", action = wezterm.action.ActivateTab(2) },
-	{ mods = "LEADER", key = "4", action = wezterm.action.ActivateTab(3) },
-	{ mods = "LEADER", key = "5", action = wezterm.action.ActivateTab(4) },
-	{ mods = "LEADER", key = "6", action = wezterm.action.ActivateTab(5) },
-	{ mods = "LEADER", key = "7", action = wezterm.action.ActivateTab(6) },
-	{ mods = "LEADER", key = "8", action = wezterm.action.ActivateTab(7) },
-	{ mods = "LEADER", key = "9", action = wezterm.action.ActivateTab(8) },
-	{ mods = "NONE", key = "F1", action = wezterm.action.ActivateTab(0) },
-	{ mods = "NONE", key = "F2", action = wezterm.action.ActivateTab(1) },
-	{ mods = "NONE", key = "F3", action = wezterm.action.ActivateTab(2) },
-	{ mods = "NONE", key = "F4", action = wezterm.action.ActivateTab(3) },
-	{ mods = "NONE", key = "F5", action = wezterm.action.ActivateTab(4) },
-	{ mods = "NONE", key = "F6", action = wezterm.action.ActivateTab(5) },
-	{ mods = "NONE", key = "F7", action = wezterm.action.ActivateTab(6) },
-	{ mods = "NONE", key = "F8", action = wezterm.action.ActivateTab(7) },
-	{ mods = "NONE", key = "F9", action = wezterm.action.ActivateTab(8) },
-}
-
 return {
 	font = wezterm.font_with_fallback({
 		{ family = "Berkeley Mono" },
@@ -52,7 +26,6 @@ return {
 	-- keybindings
 	-- disable_default_key_bindings = true,
 	leader = { mods = "CTRL", key = "t", timeout_milliseconds = 1000 },
-	keys = keyconfig,
 
 	color_scheme = "Catppuccin Mocha",
 }
