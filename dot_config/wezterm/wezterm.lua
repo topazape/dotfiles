0e9109c4f70eed9dfe1809f6b6814d7146c8efe3
topazape <topazape@gmail.com>
2023-11-22 22:45:17 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,33 +1,4 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
--- The filled in variant of the < symbol
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-
--- The filled in variant of the > symbol
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-
-config.tab_bar_style = {
-	active_tab_left = wezterm.format({
-		{ Background = { Color = "#0b0022" } },
-		{ Foreground = { Color = "#2b2042" } },
-		{ Text = SOLID_LEFT_ARROW },
-	}),
-	active_tab_right = wezterm.format({
-		{ Background = { Color = "#0b0022" } },
-		{ Foreground = { Color = "#2b2042" } },
-		{ Text = SOLID_RIGHT_ARROW },
-	}),
-	inactive_tab_left = wezterm.format({
-		{ Background = { Color = "#0b0022" } },
-		{ Foreground = { Color = "#1b1032" } },
-		{ Text = SOLID_LEFT_ARROW },
-	}),
-	inactive_tab_right = wezterm.format({
-		{ Background = { Color = "#0b0022" } },
-		{ Foreground = { Color = "#1b1032" } },
-		{ Text = SOLID_RIGHT_ARROW },
-	}),
-}
 
 return {
 	color_scheme = "Catppuccin Mocha",
