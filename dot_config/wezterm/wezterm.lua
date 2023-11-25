cf93d567550d360e1772612a253caab080c1c6ac
topazape <topazape@gmail.com>
2023-11-25 19:33:10 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -15,8 +15,8 @@ return {
 
 	-- look and feel
 	window_frame = {
-		border_left_width = "0.1cell",
-		border_right_width = "0.1cell",
+		border_left_width = "0.25cell",
+		border_right_width = "0.25cell",
 		border_bottom_height = "0.25cell",
 		border_top_height = "0.25cell",
 		border_left_color = "purple",
