14191295c904563c40ad40c30126f9bf831e2087
topazape <topazape@gmail.com>
2023-12-07 10:55:56 +0900

    Update .config/zsh/local.zsh

--- dot_config/zsh/local.zsh
+++ dot_config/zsh/local.zsh
@@ -121,6 +121,7 @@ if type aws > /dev/null 2>&1; then
 fi
 ## google-cloud-sdk
 if [[ -d $(brew --prefix)/share/google-cloud-sdk ]]; then
+  echo "yes"
   source $(brew --prefix)/share/google-cloud-sdk/path.zsh.inc
   source $(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc
 fi
