80bce1ac04f2a48fae3967deb7c6b26b9c24637c
topazape <topazape@gmail.com>
2023-12-20 18:22:41 +0900

    Update .config/wezterm/status.lua

--- dot_config/wezterm/status.lua
+++ dot_config/wezterm/status.lua
@@ -2,6 +2,8 @@ local wezterm = require("wezterm")
 
 local DEFAULT_FG = { Color = "#9a9eab" }
 local DEFAULT_BG = { Color = "#333333" }
+local SPACE_1 = " "
+local SPACE_3 = "   "
 
 local HEADER_HOST = { Foreground = { Color = "#75b1a9" }, Text = "" }
 local HEADER_CWD = { Foreground = { Color = "#92aac7" }, Text = "" }
@@ -9,47 +11,66 @@ local HEADER_DATE = { Foreground = { Color = "#ffccac" }, Text = "󱪺" }
 local HEADER_TIME = { Foreground = { Color = "#bcbabe" }, Text = "" }
 local HEADER_BATTERY = { Foreground = { Color = "#dfe166" }, Text = "" }
 
-local function get_date(elems)
-	add_element(elems, HEADER_DATE, require("wezterm").strftime("%a %b %-d"))
+local function AddElement(elems, header, str)
+	table.insert(elems, { Foreground = header.Foreground })
+	table.insert(elems, { Background = DEFAULT_BG })
+	table.insert(elems, { Text = header.Text .. SPACE_1 })
+
+	table.insert(elems, { Foreground = DEFAULT_FG })
+	table.insert(elems, { Background = DEFAULT_BG })
+	table.insert(elems, { Text = str .. SPACE_3 })
 end
 
-local function get_time(elems)
-	add_element(elems, HEADER_TIME, require("wezterm").strftime("%H:%M"))
+local function GetHostAndCwd(elems, pane)
+	local uri = pane:get_current_working_dir()
+
+	if not uri then
+		return
+	end
+
+	local cwd_uri = uri:sub(8)
+	local slash = cwd_uri:find("/")
+
+	if not slash then
+		return
+	end
+
+	local host = cwd_uri:sub(1, slash - 1)
+	local dot = host:find("[.]")
+
+	AddElement(elems, HEADER_HOST, dot and host:sub(1, dot - 1) or host)
+	AddElement(elems, HEADER_CWD, cwd_uri:sub(slash))
 end
 
-local function get_battery(elems)
-	for _, b in ipairs(require("wezterm").battery_info()) do
-		add_element(elems, HEADER_BATTERY, string.format("%.0f%%", b.state_of_charge * 100))
+local function GetDate(elems)
+	AddElement(elems, HEADER_DATE, wezterm.strftime("%a %b %-d"))
+end
+
+local function GetTime(elems)
+	AddElement(elems, HEADER_TIME, wezterm.strftime("%H:%M"))
+end
+
+local function GetBattery(elems, window)
+	if not window:get_dimensions().is_full_screen then
+		return
+	end
+
+	for _, b in ipairs(wezterm.battery_info()) do
+		AddElement(elems, HEADER_BATTERY, string.format("%.0f%%", b.state_of_charge * 100))
 	end
 end
 
-local function add_element(elems, header, str)
-	table.insert(elems, { Foreground = header.Foreground })
-	table.insert(elems, { Background = DEFAULT_BG })
-	table.insert(elems, { Text = header.Text .. " " })
-	table.insert(elems, { Foreground = DEFAULT_FG })
-	table.insert(elems, { Background = DEFAULT_BG })
-	table.insert(elems, { Text = str .. "   " })
-end
-
-local function right_update(window, pane)
+local function RightUpdate(window, pane)
 	local elems = {}
 
-	get_date(elems)
-	get_time(elems)
-	get_battery(elems)
+	GetHostAndCwd(elems, pane)
+	GetDate(elems)
+	GetBattery(elems, window)
+	GetTime(elems)
 
-	window:set_status(require("wezterm").format(elems))
+	window:set_right_status(wezterm.format(elems))
 end
 
-require("wezterm").on("update-status", function(window, pane)
-	local date = wezterm.strftime("%Y-%m-%d %H:%M:%S")
-
-	-- Make it italic and underlined
-	window:set_right_status(wezterm.format({
-		{ Attribute = { Underline = "Single" } },
-		{ Attribute = { Italic = true } },
-		{ Text = "Hello " .. date },
-	}))
-	-- right_update(window, pane)
+wezterm.on("update-status", function(window, pane)
+	RightUpdate(window, pane)
 end)
