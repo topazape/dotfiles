37f6ce8b6b12f04e48c09d30ab68b39d25aa68fb
topazape <topazape@gmail.com>
2023-12-20 18:16:59 +0900

    Add .config/wezterm/status.lua

---
+++ dot_config/wezterm/status.lua
@@ -0,0 +1,45 @@
+local DEFAULT_FG = { Color = "#9a9eab" }
+local DEFAULT_BG = { Color = "#333333" }
+
+local HEADER_HOST = { Foreground = { Color = "#75b1a9" }, Text = "" }
+local HEADER_CWD = { Foreground = { Color = "#92aac7" }, Text = "" }
+local HEADER_DATE = { Foreground = { Color = "#ffccac" }, Text = "󱪺" }
+local HEADER_TIME = { Foreground = { Color = "#bcbabe" }, Text = "" }
+local HEADER_BATTERY = { Foreground = { Color = "#dfe166" }, Text = "" }
+
+local function get_date(elems)
+	add_element(elems, HEADER_DATE, require("wezterm").strftime("%a %b %-d"))
+end
+
+local function get_time(elems)
+	add_element(elems, HEADER_TIME, require("wezterm").strftime("%H:%M"))
+end
+
+local function get_battery(elems)
+	for _, b in ipairs(require("wezterm").battery_info()) do
+		add_element(elems, HEADER_BATTERY, string.format("%.0f%%", b.state_of_charge * 100))
+	end
+end
+
+local function add_element(elems, header, str)
+	table.insert(elems, { Foreground = header.Foreground })
+	table.insert(elems, { Background = DEFAULT_BG })
+	table.insert(elems, { Text = header.Text .. " " })
+	table.insert(elems, { Foreground = DEFAULT_FG })
+	table.insert(elems, { Background = DEFAULT_BG })
+	table.insert(elems, { Text = str .. "   " })
+end
+
+local function right_update(window, pane)
+	local elems = {}
+
+	get_date(elems)
+	get_time(elems)
+	get_battery(elems)
+
+	window:set_status(require("wezterm").format(elems))
+end
+
+require("wezterm").on("update-status", function(window, pane)
+	right_update(window, pane)
+end)
