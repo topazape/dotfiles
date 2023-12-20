f731b19f078687dafa0a46c6e35ebf14c59420ee
topazape <topazape@gmail.com>
2023-12-20 18:37:22 +0900

    Update .config/wezterm/status.lua

--- dot_config/wezterm/status.lua
+++ dot_config/wezterm/status.lua
@@ -1,13 +1,13 @@
 local wezterm = require("wezterm")
 
 local DEFAULT_FG = { Color = "#9a9eab" }
-local DEFAULT_BG = { Color = "#333333" }
+local DEFAULT_BG = { Color = "#222222" }
 
 local HEADER_DATE = { Foreground = { Color = "#ffccac" }, Text = "󱪺" }
 local HEADER_TIME = { Foreground = { Color = "#bcbabe" }, Text = "" }
 local HEADER_BATTERY = { Foreground = { Color = "#dfe166" }, Text = "" }
 
-local function AddElement(elems, header, str)
+local function add_element(elems, header, str)
 	table.insert(elems, { Foreground = header.Foreground })
 	table.insert(elems, { Background = DEFAULT_BG })
 	table.insert(elems, { Text = header.Text .. " " })
@@ -18,20 +18,20 @@ local function AddElement(elems, header, str)
 end
 
 local function get_date(elems)
-	AddElement(elems, HEADER_DATE, wezterm.strftime("%Y-%m-%d %a"))
+	add_element(elems, HEADER_DATE, wezterm.strftime("%Y-%m-%d %a"))
 end
 
 local function get_time(elems)
-	AddElement(elems, HEADER_TIME, wezterm.strftime("%H:%M"))
+	add_element(elems, HEADER_TIME, wezterm.strftime("%H:%M"))
 end
 
 local function get_battery(elems)
 	for _, b in ipairs(wezterm.battery_info()) do
-		AddElement(elems, HEADER_BATTERY, string.format("%.0f%%", b.state_of_charge * 100))
+		add_element(elems, HEADER_BATTERY, string.format("%.0f%%", b.state_of_charge * 100))
 	end
 end
 
-local function RightUpdate(window)
+local function right_update(window)
 	local elems = {}
 
 	get_time(elems)
@@ -42,5 +42,5 @@ local function RightUpdate(window)
 end
 
 wezterm.on("update-status", function(window)
-	RightUpdate(window)
+	right_update(window)
 end)
