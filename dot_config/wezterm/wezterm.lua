1aa1be85d60a0bfc48b93c45e12b44fca7325516
topazape <topazape@gmail.com>
2024-01-12 19:56:32 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -26,7 +26,7 @@ return {
 	adjust_window_size_when_changing_font_size = false,
 
 	-- look and feel
-	window_background_opacity = 0.93,
+	window_background_opacity = 0.90,
 	macos_window_background_blur = 20,
 	use_fancy_tab_bar = false,
 
