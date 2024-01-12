733cd126697c576c4fcc6ac5bd546c4a5303884d
topazape <topazape@gmail.com>
2024-01-12 15:04:41 +0900

    Update .config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -40,6 +40,19 @@ local function library(plugins)
 	return paths
 end
 
+-- python path for pylsp
+local py_path = nil
+if vim.fn.executable("pylsp") then
+	local venv_path = os.getenv("VIRTUAL_ENV")
+	if venv_path ~= nil then
+		py_path = venv_path .. "/bin/python3"
+	elseif vim.g.python3_host_prog ~= nil then
+		py_path = vim.g.python3_host_prog
+	else
+		py_path = vim.fn.exepath("python3")
+	end
+end
+
 local handlers = {
 	-- The first entry (without a key) will be the default handler
 	-- and will be called for each installed server that doesn"t have
@@ -61,6 +74,36 @@ local handlers = {
 					},
 					diagnostics = {
 						globals = { "vim" },
+					},
+				},
+			},
+		})
+	end,
+
+	["pylsp"] = function()
+		require("lspconfig").pylsp.setup({
+			settings = {
+				pylsp = {
+					plugins = {
+						-- python-lsp-server[all] plugins will be disabled
+						autopep8 = { enabled = false },
+						flake8 = { enabled = false },
+						mccabe = { enabled = false },
+						preload = { enabled = false },
+						pycodestyle = { enabled = false },
+						pydocstyle = { enabled = false },
+						pyflakes = { enabled = false },
+						pylint = { enabled = false },
+						rope_autocomplete = { enabled = false },
+						rope_completion = { enabled = false },
+						yapf = { enabled = false },
+						-- 3rd-party plugins
+						---- type checker
+						pylsp_mypy = {
+							enabled = true,
+							overrides = { "--python-executable", py_path, true },
+							report_progress = true,
+						},
 					},
 				},
 			},
