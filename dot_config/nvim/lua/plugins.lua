8000666f2f92cd2c68f59d15f6588fee5f5c8cc2
topazape <topazape@gmail.com>
2024-01-17 10:44:08 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				flavour = "latte", -- latte, frappe, macchiato, mocha
 				integrations = {
 					cmp = true,
 					dap = true,
