44ac9dea63ffe7cf775ba12c0a3ad9d73ffd16a3
topazape <topazape@gmail.com>
2024-01-18 19:01:13 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -20,7 +20,6 @@ return {
 	font = wezterm.font_with_fallback({
 		{ family = "Berkeley Mono" },
 		{ family = "Pretendard JP" },
-		{ family = "BlexMono Nerd Font Mono" },
 	}),
 	font_size = 14,
 	warn_about_missing_glyphs = false,
