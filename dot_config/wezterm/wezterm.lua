c7f3b81e26ba9c5089ad65d0d8f91a51663204be
topazape <topazape@gmail.com>
2024-01-19 10:41:26 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -19,6 +19,7 @@ return {
 	-- font
 	font = wezterm.font_with_fallback({
 		{ family = "Berkeley Mono" },
+		{ family = "Noto Sans Mono CJK JP" },
 		{ family = "Pretendard JP" },
 	}),
 	font_size = 14,
