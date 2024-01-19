ab46ebc39597e0fe5b81f0f2d1317d0c9aae2c80
topazape <topazape@gmail.com>
2024-01-19 15:07:40 +0900

    Update .config/nvim/lua/config/copilot.lua

--- dot_config/nvim/lua/config/copilot.lua
+++ dot_config/nvim/lua/config/copilot.lua
@@ -2,14 +2,26 @@ local copilot = require("copilot")
 
 copilot.setup({
 	panel = {
-		enabled = false,
+		enabled = true,
+		auto_refresh = false,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>",
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4,
+		},
 	},
 	suggestion = {
 		enabled = true,
-		auto_trigger = true,
-		debounce = 100,
+		auto_trigger = false,
+		debounce = 75,
 		keymap = {
-			accept = "<TAB>",
+			accept = "<M-l>",
 			accept_word = false,
 			accept_line = false,
 			next = "<M-]>",
@@ -18,14 +30,16 @@ copilot.setup({
 		},
 	},
 	filetypes = {
-		cvs = false,
+		yaml = false,
+		markdown = false,
+		help = false,
 		gitcommit = false,
 		gitrebase = false,
-		help = false,
 		hgcommit = false,
-		markdown = true,
 		svn = false,
-		yaml = true,
+		cvs = false,
 		["."] = false,
 	},
+	copilot_node_command = "node", -- Node.js version must be > 18.x
+	server_opts_overrides = {},
 })
