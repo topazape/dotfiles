ced44147718cc67c8ff44709eacd6d6975c967e9
topazape <topazape@gmail.com>
2024-01-25 10:03:20 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -222,42 +222,42 @@ return {
 
 	-- git
 	---- gitsigns
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { text = "│" },
-					change = { text = "│" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "┆" },
-				},
-				current_line_blame = false,
-				current_line_blame_opts = {
-					virt_text = true,
-					virt_text_pos = "eol",
-					delay = 100,
-					ignore_whitespace = true,
-				},
-
-				on_attach = function(bufnr)
-					local gs = package.loaded.gitsigns
-					local function map(mode, l, r, opts)
-						opts = opts or {}
-						opts.buffer = bufnr
-						vim.keymap.set(mode, l, r, opts)
-					end
-					-- Actions
-					map("n", "<leader>hb", function()
-						gs.blame_line({ full = true })
-					end)
-					map("n", "<leader>tb", gs.toggle_current_line_blame)
-				end,
-			})
-		end,
-	},
+	-- {
+	-- 	"lewis6991/gitsigns.nvim",
+	-- 	config = function()
+	-- 		require("gitsigns").setup({
+	-- 			signs = {
+	-- 				add = { text = "│" },
+	-- 				change = { text = "│" },
+	-- 				delete = { text = "_" },
+	-- 				topdelete = { text = "‾" },
+	-- 				changedelete = { text = "~" },
+	-- 				untracked = { text = "┆" },
+	-- 			},
+	-- 			current_line_blame = false,
+	-- 			current_line_blame_opts = {
+	-- 				virt_text = true,
+	-- 				virt_text_pos = "eol",
+	-- 				delay = 100,
+	-- 				ignore_whitespace = true,
+	-- 			},
+	--
+	-- 			on_attach = function(bufnr)
+	-- 				local gs = package.loaded.gitsigns
+	-- 				local function map(mode, l, r, opts)
+	-- 					opts = opts or {}
+	-- 					opts.buffer = bufnr
+	-- 					vim.keymap.set(mode, l, r, opts)
+	-- 				end
+	-- 				-- Actions
+	-- 				map("n", "<leader>hb", function()
+	-- 					gs.blame_line({ full = true })
+	-- 				end)
+	-- 				map("n", "<leader>tb", gs.toggle_current_line_blame)
+	-- 			end,
+	-- 		})
+	-- 	end,
+	-- },
 	---- conflict
 	{
 		"akinsho/git-conflict.nvim",
