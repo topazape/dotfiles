a8948b362241cd8b902191e94f84145e926c852e
topazape <topazape@gmail.com>
2024-01-28 20:10:54 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -20,7 +20,7 @@ return {
 	font = wezterm.font_with_fallback({
 		{ family = "Berkeley Mono" },
 		{ family = "Pretendard JP" },
-		{ family = "DejeVu Sans Mono" },
+		{ family = "DejaVu Sans Mono" },
 	}),
 	font_size = 14,
 	warn_about_missing_glyphs = false,
