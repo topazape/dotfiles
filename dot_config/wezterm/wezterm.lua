da45a008f823ec724471566efff0e5f3e33dc5c3
topazape <topazape@gmail.com>
2024-01-28 20:08:04 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -20,7 +20,6 @@ return {
 	font = wezterm.font_with_fallback({
 		{ family = "Berkeley Mono" },
 		{ family = "Pretendard JP" },
-		{ family = "DejaVu Sans Mono" }, -- for check mark
 	}),
 	font_size = 14,
 	warn_about_missing_glyphs = false,
