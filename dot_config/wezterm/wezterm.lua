2d77fefee2b68936d6d338f9e7b758bbb268fe49
topazape <topazape@gmail.com>
2024-01-28 20:06:45 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -21,6 +21,7 @@ return {
 		{ family = "Berkeley Mono" },
 		{ family = "Pretendard JP" },
 		{ family = "DejaVu Sans Mono" }, -- for check mark
+		{ family = "Apple Color Emoji" },
 	}),
 	font_size = 14,
 	warn_about_missing_glyphs = false,
