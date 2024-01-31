0751de2772c382da00a2934d59931723e329de91
topazape <topazape@gmail.com>
2024-01-31 16:29:43 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -28,7 +28,7 @@ return {
 	-- look and feel
 	window_background_opacity = 0.90,
 	macos_window_background_blur = 20,
-	use_fancy_tab_bar = false,
+	use_fancy_tab_bar = true,
 
 	-- bell
 	audible_bell = "Disabled",
