b6d1fadb25f9cccb8292e2a7f20332a365d8f4e3
topazape <topazape@gmail.com>
2024-02-08 16:11:31 +0900

    Update .config/nvim/lua/config/format-on-save.lua

--- dot_config/nvim/lua/config/format-on-save.lua
+++ dot_config/nvim/lua/config/format-on-save.lua
@@ -6,18 +6,32 @@ local XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME")
 if not XDG_CONFIG_HOME then
 	XDG_CONFIG_HOME = os.getenv("HOME") .. "/.config"
 end
+-- xdg cache dir
+local XDG_CACHE_HOME = os.getenv("XDG_CACHE_HOME")
+if not XDG_CACHE_HOME then
+	XDG_CACHE_HOME = os.getenv("HOME") .. "/.cache"
+end
 
 local ruff_cmd = { "ruff", "format", "--no-cache", "--respect-gitignore", "-" }
 if vim.fn.filereadable(XDG_CONFIG_HOME .. "/ruff/ruff.toml") == 1 then
-	ruff_cmd =
-		{ "ruff", "format", "--config", XDG_CONFIG_HOME .. "/ruff/ruff.toml", "--no-cache", "--respect-gitignore", "-" }
+	ruff_cmd = {
+		"ruff",
+		"format",
+		"--config",
+		XDG_CONFIG_HOME .. "/ruff/ruff.toml",
+		"--no-cache",
+		"--respect-gitignore",
+		"-",
+	}
 end
+
+vim.env.GOLANGCI_LINT_CACHE = XDG_CACHE_HOME .. "/golangci-lint"
+local golangci_lint_cmd = { "golangci-lint", "run", "--fix", "--out-format", "json", "-" }
 
 format_on_save.setup({
 	formatter_by_ft = {
 		go = {
-			formatters.shell({ cmd = { "goimports" } }),
-			formatters.shell({ cmd = { "gofumpt" } }),
+			formatters.shell({ cmd = golangci_lint_cmd }),
 		},
 		lua = {
 			formatters.stylua, -- default
