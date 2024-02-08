729906d66476d1540b4525911725e4478822bbfe
topazape <topazape@gmail.com>
2024-02-08 15:57:49 +0900

    Update .config/nvim/lua/config/format-on-save.lua

--- dot_config/nvim/lua/config/format-on-save.lua
+++ dot_config/nvim/lua/config/format-on-save.lua
@@ -1,6 +1,18 @@
 local format_on_save = require("format-on-save")
 local formatters = require("format-on-save.formatters")
 
+-- xdg config dir
+local XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME")
+if not XDG_CONFIG_HOME then
+	XDG_CONFIG_HOME = os.getenv("HOME") .. "/.config"
+end
+
+local ruff_cmd = { "ruff", "format", "--no-cache", "--respect-gitignore", "-" }
+if vim.fn.filereadable(XDG_CONFIG_HOME .. "/ruff/ruff.toml") == 1 then
+	ruff_cmd =
+		{ "ruff", "format", "--config", XDG_CONFIG_HOME .. "/ruff/ruff.toml", "--no-cache", "--respect-gitignore", "-" }
+end
+
 format_on_save.setup({
 	formatter_by_ft = {
 		go = {
@@ -12,7 +24,7 @@ format_on_save.setup({
 		},
 		python = {
 			formatters.shell({ cmd = { "isort", "-" } }),
-			formatters.shell({ cmd = { "ruff", "format", "--no-cache", "--respect-gitignore", "-" } }),
+			formatters.shell({ cmd = ruff_cmd }),
 		},
 		rust = {
 			formatters.shell({ cmd = { "rustfmt", "--emit=stdout" } }),
