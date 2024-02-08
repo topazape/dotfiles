b97fd6785373c54202bc3f1308aaf7e2dc550570
topazape <topazape@gmail.com>
2024-02-08 20:48:39 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -19,7 +19,7 @@ return {
 	-- font
 	font = wezterm.font_with_fallback({
 		{ family = "Berkeley Mono" },
-		{ family = "Pretendard JP" },
+		{ family = "IBM Plex Sans JP" },
 	}),
 	font_size = 14,
 	warn_about_missing_glyphs = false,
