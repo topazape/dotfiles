edf6a4f77ef75e4ec76d31da84ee85acc62f388a
topazape <topazape@gmail.com>
2024-02-11 20:55:31 +0900

    Update .config/nvim/lua/config/format-on-save.lua

--- dot_config/nvim/lua/config/format-on-save.lua
+++ dot_config/nvim/lua/config/format-on-save.lua
@@ -23,7 +23,7 @@ end
 format_on_save.setup({
 	formatter_by_ft = {
 		go = {
-			-- formatters.lsp,
+			formatters.lsp,
 			formatters.shell({ cmd = { "gci", "print" } }),
 			-- formatters.shell({ cmd = { "gofumpt" } }),
 		},
