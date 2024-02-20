16c5ec49ec3cf401c0903d909b469626314472b3
topazape <topazape@gmail.com>
2024-02-20 11:08:39 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -18,7 +18,7 @@ return {
 
 	-- font
 	font = wezterm.font_with_fallback({
-		{ family = "Berkeley Mono" },
+		{ family = "Berkeley Mono", scale = 1.5 },
 		{ family = "IBM Plex Sans JP" },
 	}),
 	font_size = 14,
