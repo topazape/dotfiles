36cc5506e30e0d7a8502e0320ce42807ca27c4ec
topazape <topazape@gmail.com>
2024-02-20 15:17:13 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -77,22 +77,23 @@ return {
 	},
 
 	-- completion
-	-- {
-	-- 	"hrsh7th/nvim-cmp",
-	-- 	dependencies = {
-	-- 		"hrsh7th/cmp-buffer",
-	-- 		"hrsh7th/cmp-nvim-lsp",
-	-- 		"hrsh7th/cmp-nvim-lua",
-	-- 		"hrsh7th/cmp-path",
-	-- 		"hrsh7th/cmp-vsnip",
-	-- 		"hrsh7th/vim-vsnip",
-	-- 		"hrsh7th/vim-vsnip-integ",
-	-- 	},
-	-- 	event = "InsertEnter",
-	-- 	config = function()
-	-- 		require("config/cmp")
-	-- 	end,
-	-- },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/vim-vsnip-integ",
+		},
+		event = "InsertEnter",
+		config = function()
+			require("config/cmp")
+		end,
+	},
+
 	-- AI
 	---- copilot
 	{
