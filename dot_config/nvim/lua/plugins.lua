e740bb2a515515691d59f4a70c3cff66c4a31699
topazape <topazape@gmail.com>
2024-03-07 08:35:00 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,34 +1,5 @@
--- rosewater = "#f4dbd6",
--- flamingo = "#f0c6c6",
--- pink = "#f5bde6",
--- mauve = "#c6a0f6",
--- red = "#ed8796",
--- maroon = "#ee99a0",
--- peach = "#f5a97f",
--- yellow = "#eed49f",
--- green = "#a6da95",
--- teal = "#8bd5ca",
--- sky = "#91d7e3",
--- sapphire = "#7dc4e4",
--- blue = "#8aadf4",
--- lavender = "#b7bdf8",
--- text = "#cad3f5",
--- subtext1 = "#b8c0e0",
--- subtext0 = "#a5adcb",
--- overlay2 = "#939ab7",
--- overlay1 = "#8087a2",
--- overlay0 = "#6e738d",
--- surface2 = "#5b6078",
--- surface1 = "#494d64",
--- surface0 = "#363a4f",
--- base = "#24273a",
--- mantle = "#1e2030",
--- crust = "#181926",
-
 local M = {}
-local flavour = "macchiato"
-
-M.config = function()
+M.config = function(flavour)
 	require("catppuccin").setup({
 		flavour = flavour,
 		integrations = {
@@ -56,7 +27,7 @@ return {
 	{
 		"catppuccin/nvim",
 		priority = 1000,
-		config = M.config,
+		config = M.config("macchiato"),
 	},
 	-- icons
 	{
