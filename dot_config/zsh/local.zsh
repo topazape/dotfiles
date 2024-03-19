898c771af827a6fe82e1157aea123c2c05ab8881
topazape <topazape@gmail.com>
2024-03-19 23:29:54 +0900

    Update .config/zsh/local.zsh

--- dot_config/zsh/local.zsh
+++ dot_config/zsh/local.zsh
@@ -112,7 +112,7 @@ if type eza > /dev/null 2>&1; then
   alias ls="eza -F --git"
   alias la="eza -a --git"
   alias ll="eza -ahlF --git"
-  alias tree="eza -T"
+  alias tree="eza -TF"
 fi
 
 # Cloud
