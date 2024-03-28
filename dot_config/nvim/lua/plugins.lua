7cb543ef04256daf9c1bd3a9ce9851df34f76c46
topazape <topazape@gmail.com>
2024-03-28 22:59:51 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -313,18 +313,6 @@ return {
 			require("gitlinker").setup()
 		end,
 	},
-	---- GitHub
-	{
-		"pwntester/octo.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("octo").setup()
-		end,
-	},
 
 	-- markdown
 	{
