fbc0378a38e83682f8e625f7ff26921c57bf4d8e
topazape <topazape@gmail.com>
2024-07-04 19:42:00 +0900

    Update .config/zsh/local.zsh

--- dot_config/zsh/local.zsh
+++ dot_config/zsh/local.zsh
@@ -131,7 +131,6 @@ fi
 ### rye
 if type rye > /dev/null 2>&1; then
   export RYE_HOME=${XDG_DATA_HOME}/rye
-  export PIP_TOOLS_CACHE_DIR=${XDG_CACHE_HOME}/pip-tools
   export UV_CACHE_DIR=${XDG_CACHE_HOME}/uv
 fi
 ## Rust
