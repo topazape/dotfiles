6d7443a69feae0eae036c96856b0242837db426e
topazape <topazape@gmail.com>
2024-07-09 17:36:05 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -29,7 +29,7 @@ return {
 	line_height = 1.0,
 
 	-- look and feel
-	window_background_opacity = 1.00,
+	window_background_opacity = 0.00,
 	macos_window_background_blur = 20,
 	use_fancy_tab_bar = false,
 
