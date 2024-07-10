227cde8a0d96b3ea1f0cc40bc48b10813d47f209
topazape <topazape@gmail.com>
2024-07-10 17:57:06 +0900

    Add .config/nvim/lua/config/conform.lua

---
+++ dot_config/nvim/lua/config/conform.lua
@@ -0,0 +1,42 @@
+conform = require("conform")
+
+conform.setup({
+  lsp_format = "first",
+  formatters_by_ft = {
+    go = {
+      "goimports",
+      "gofumpt",
+    },
+    lua = {
+      "stylua",
+    },
+    python = {
+      "ruff_organize_imports",
+      "ruff_format",
+    },
+    rust = {
+      "rustfmt",
+    },
+    sh = {
+      "shfmt",
+    },
+    bash = {
+      "shfmt",
+    },
+    zsh = {
+      "shfmt",
+    },
+    json = {
+      "prettier",
+    },
+    toml = {
+      "taplo",
+    },
+    yaml = {
+      "prettier",
+    },
+    markdown = {
+      "prettier",
+    },
+  }
+})
