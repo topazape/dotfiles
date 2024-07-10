f26a8ffce1e4fe85a6c7f64c482801e289c0ba01
topazape <topazape@gmail.com>
2024-07-10 18:19:15 +0900

    Update .config/nvim/lua/config/conform.lua

--- dot_config/nvim/lua/config/conform.lua
+++ dot_config/nvim/lua/config/conform.lua
@@ -2,42 +2,44 @@ local conform = require("conform")
 print("conform loaded")
 
 conform.setup({
-  format_on_save = {
-    lsp_format = "first",
-  },
-  formatters_by_ft = {go = {
-      "goimports", "gofumpt",
-    },
-    lua = {
-      "stylua",
-    },
-    python = {
-      "ruff_organize_imports",
-      "ruff_format",
-    },
-    rust = {
-      "rustfmt",
-    },
-    sh = {
-      "shfmt",
-    },
-    bash = {
-      "shfmt",
-    },
-    zsh = {
-      "shfmt",
-    },
-    json = {
-      "prettier",
-    },
-    toml = {
-      "taplo",
-    },
-    yaml = {
-      "prettier",
-    },
-    markdown = {
-      "prettier",
-    },
-  }
+	format_on_save = {
+		lsp_format = "first",
+	},
+	formatters_by_ft = {
+		go = {
+			"goimports",
+			"gofumpt",
+		},
+		lua = {
+			"stylua",
+		},
+		python = {
+			"ruff_organize_imports",
+			"ruff_format",
+		},
+		rust = {
+			"rustfmt",
+		},
+		sh = {
+			"shfmt",
+		},
+		bash = {
+			"shfmt",
+		},
+		zsh = {
+			"shfmt",
+		},
+		json = {
+			"prettier",
+		},
+		toml = {
+			"taplo",
+		},
+		yaml = {
+			"prettier",
+		},
+		markdown = {
+			"prettier",
+		},
+	},
 })
