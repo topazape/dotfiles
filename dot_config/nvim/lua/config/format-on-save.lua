9285dcaa4cf35d9b25ba4e0a34635fbcb3a0513e
topazape <topazape@gmail.com>
2024-07-10 10:31:18 +0900

    Update .config/nvim/lua/config/format-on-save.lua

--- dot_config/nvim/lua/config/format-on-save.lua
+++ dot_config/nvim/lua/config/format-on-save.lua
@@ -56,7 +56,7 @@ format_on_save.setup({
 			formatters.shell({ cmd = { "yamlfmt", "-" } }),
 		},
 		markdown = {
-			formatters.shell({ cmd = { "mdslw", "-" } }),
+			formatters.shell({ cmd = { "mdformat", "-" } }),
 		},
 		typst = {
 			formatters.lsp, -- tinymist.typstyle
