138ae93b03c27eca9c5bd092045ae766cf31aaf7
topazape <topazape@gmail.com>
2024-07-11 22:33:44 +0900

    Update .config/nvim/lua/config/conform.lua

--- dot_config/nvim/lua/config/conform.lua
+++ dot_config/nvim/lua/config/conform.lua
@@ -2,7 +2,8 @@ local conform = require("conform")
 
 conform.setup({
 	format_on_save = {
-		lsp_format = "first",
+		timeout_ms = 500,
+		lsp_format = "fallback",
 	},
 	formatters_by_ft = {
 		["*"] = {
