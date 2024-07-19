21a7e824256a1ebec0b1cd05602e8c8107b12511
topazape <topazape@gmail.com>
2024-07-19 09:39:48 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -94,17 +94,21 @@ return {
 
 	-- completion
 	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-		},
-		event = "InsertEnter",
-		config = function()
-			require("config/cmp")
-		end,
+		"nvimdev/epo.nvim",
+		config = true,
 	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		"hrsh7th/cmp-buffer",
+	-- 		"hrsh7th/cmp-path",
+	-- 	},
+	-- 	event = "InsertEnter",
+	-- 	config = function()
+	-- 		require("config/cmp")
+	-- 	end,
+	-- },
 
 	-- AI
 	---- copilot
