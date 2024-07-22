073b036d0a543d3892e78089a956c211de01ec38
topazape <topazape@gmail.com>
2024-07-22 16:11:14 +0900

    Update .config/nvim/lua/config/copilot.lua

--- dot_config/nvim/lua/config/copilot.lua
+++ dot_config/nvim/lua/config/copilot.lua
@@ -27,7 +27,7 @@ copilot.setup({
 		svn = false,
 		text = false,
 		yaml = false,
-		rust = true,
+		rust = false,
 		["."] = false,
 	},
 	copilot_node_command = "node", -- Node.js version must be > 18.x
