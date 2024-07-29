832d42e53be1a517e3aebe64772170b521416c06
topazape <topazape@gmail.com>
2024-07-29 12:25:30 +0900

    Update .config/nvim/lua/config/gitsigns.lua

--- dot_config/nvim/lua/config/gitsigns.lua
+++ dot_config/nvim/lua/config/gitsigns.lua
@@ -1,33 +1,17 @@
 local gitsigns = require("gitsigns")
 
 gitsigns.setup({
-	signs = {
-		add = { text = "│" },
-		change = { text = "│" },
-		delete = { text = "_" },
-		topdelete = { text = "‾" },
-		changedelete = { text = "~" },
-		untracked = { text = "┆" },
-	},
-	current_line_blame = false,
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
+	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
-		virt_text_pos = "eol",
-		delay = 100,
-		ignore_whitespace = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+		ignore_whitespace = false,
+		virt_text_priority = 100,
 	},
-
-	on_attach = function(bufnr)
-		local gs = package.loaded.gitsigns
-		local function map(mode, l, r, opts)
-			opts = opts or {}
-			opts.buffer = bufnr
-			vim.keymap.set(mode, l, r, opts)
-		end
-		-- Actions
-		map("n", "<leader>hb", function()
-			gs.blame_line({ full = true })
-		end)
-		map("n", "<leader>tb", gs.toggle_current_line_blame)
-	end,
+	current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
 })
