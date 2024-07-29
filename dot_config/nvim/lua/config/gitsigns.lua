7925eb354823d69e00c3be707a28595922f0053b
topazape <topazape@gmail.com>
2024-07-29 12:27:05 +0900

    Remove .config/nvim/lua/config/gitsigns.lua

--- dot_config/nvim/lua/config/gitsigns.lua
+++
@@ -1,8 +0,0 @@
-local gitsigns = require("gitsigns")
-
-gitsigns.setup({
-	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-	linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
-	word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
-})
