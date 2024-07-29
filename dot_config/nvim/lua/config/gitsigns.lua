831166cc0470430690f9d1545767ace1cebacb99
topazape <topazape@gmail.com>
2024-07-29 15:14:30 +0900

    Add .config/nvim/lua/config/gitsigns.lua

---
+++ dot_config/nvim/lua/config/gitsigns.lua
@@ -0,0 +1,8 @@
+local gitsigns = require("gitsigns")
+
+gitsigns.setup({
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
+})
