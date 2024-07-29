fb39a42992b06f51db4c7edd7b5b0ebf25df260e
topazape <topazape@gmail.com>
2024-07-29 12:19:47 +0900

    Add .config/nvim/lua/config/gitsigns.lua

---
+++ dot_config/nvim/lua/config/gitsigns.lua
@@ -0,0 +1,33 @@
+local gitsigns = require("gitsigns")
+
+gitsigns.setup({
+	signs = {
+		add = { text = "│" },
+		change = { text = "│" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+		untracked = { text = "┆" },
+	},
+	current_line_blame = false,
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol",
+		delay = 100,
+		ignore_whitespace = true,
+	},
+
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+		-- Actions
+		map("n", "<leader>hb", function()
+			gs.blame_line({ full = true })
+		end)
+		map("n", "<leader>tb", gs.toggle_current_line_blame)
+	end,
+})
