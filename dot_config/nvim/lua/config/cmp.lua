70b15a402b9ee40421e04587f6268356bc313d1d
topazape <topazape@gmail.com>
2024-07-30 16:20:37 +0900

    Update .config/nvim/lua/config/cmp.lua

--- dot_config/nvim/lua/config/cmp.lua
+++ dot_config/nvim/lua/config/cmp.lua
@@ -43,11 +43,3 @@ cmp.setup({
 
 	formatting = {},
 })
-
-cmp.event:on("menu_opened", function()
-	vim.b.copilot_suggestion_hidden = true
-end)
-
-cmp.event:on("menu_closed", function()
-	vim.b.copilot_suggestion_hidden = false
-end)
