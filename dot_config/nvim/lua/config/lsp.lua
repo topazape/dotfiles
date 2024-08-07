f575f41b52dea7e1d3a36446fd2606b4693d9073
topazape <topazape@gmail.com>
2024-08-08 00:18:18 +0900

    Update .config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -198,13 +198,12 @@ local handlers = {
 				languages = {
 					python = {
 						mypy = {
-							lintCommand = "mypy --show-column-numbers",
+							lintCommand = "mypy ${INPUT} --show-column-numbers",
 							lintFormats = {
 								"%f:%l:%c: %trror: %m",
 								"%f:%l:%c: %tarning: %m",
 								"%f:%l:%c: %tote: %m",
 							},
-							lintSource = "mypy",
 						},
 					},
 				},
