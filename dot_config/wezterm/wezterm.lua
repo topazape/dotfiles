97578cf4fca468d5b628017edd04c770acde622a
topazape <topazape@gmail.com>
2024-08-08 09:44:58 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -26,7 +26,7 @@ return {
 	font_size = 15,
 	warn_about_missing_glyphs = false,
 	adjust_window_size_when_changing_font_size = false,
-	line_height = 1.05,
+	line_height = 1.1,
 
 	-- look and feel
 	window_background_opacity = 1.00,
