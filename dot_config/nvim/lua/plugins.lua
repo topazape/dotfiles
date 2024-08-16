e813d36015b7d643d1bc27ad266cb701bc53b5e2
topazape <topazape@gmail.com>
2024-08-16 11:13:07 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -28,8 +28,8 @@ return {
 		"catppuccin/nvim",
 		-- priority = 1000,
 		config = function()
-			M.config("latte")
-			-- M.config("frappe")
+			-- M.config("latte")
+			M.config("frappe")
 			-- M.config("macchiato")
 			-- M.config("mocha")
 		end,
