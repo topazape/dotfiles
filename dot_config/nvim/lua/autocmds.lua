c42ccf30542a2e8c4c684096a57ee295ab65bca6
topazape <topazape@gmail.com>
2024-09-05 13:31:28 +0900

    Update .config/nvim/lua/autocmds.lua

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -78,7 +78,6 @@ autocmd({ "FileType" }, {
 autocmd({ "FileType" }, {
 	pattern = { "sql" },
 	callback = function()
-		vim.bo.commentstring = "-- %s"
 		vim.opt_local.tabstop = 4
 		vim.opt_local.shiftwidth = 4
 		vim.opt_local.softtabstop = 4
@@ -109,7 +108,6 @@ autocmd({ "FileType" }, {
 autocmd({ "FileType" }, {
 	pattern = { "typst" },
 	callback = function()
-		vim.bo.commentstring = "// %s"
 		vim.opt_local.tabstop = 2
 		vim.opt_local.shiftwidth = 2
 		vim.opt_local.softtabstop = 2
