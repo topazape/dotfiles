fb6908b84dc79fffd8953d7b80f9d3d697f7ec3a
topazape <topazape@gmail.com>
2024-09-14 12:08:05 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -6,7 +6,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("tokyonight").setup({
-				style = "moon",
+				style = "night",
 				styles = {},
 				dim_inactive = true,
 				lualine_bold = true,
