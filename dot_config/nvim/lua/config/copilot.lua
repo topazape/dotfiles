69b3798e0a85ce40f89bb289bc78ed7653880124
topazape <topazape@gmail.com>
2024-10-06 00:05:32 +0900

    Update .config/nvim/lua/config/copilot.lua

--- dot_config/nvim/lua/config/copilot.lua
+++ dot_config/nvim/lua/config/copilot.lua
@@ -9,7 +9,7 @@ copilot.setup({
 		auto_trigger = true,
 		debounce = 100,
 		keymap = {
-			accept = "<TAB>",
+			accept = "<C-f>",
 			accept_word = false,
 			accept_line = false,
 			next = "<M-]>",
