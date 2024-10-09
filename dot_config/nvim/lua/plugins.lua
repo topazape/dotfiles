fb0ae70792bc0621f5028935004366d5c8f3d4a5
topazape <topazape@gmail.com>
2024-10-09 18:43:50 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -105,8 +105,11 @@ return {
 				},
 			},
 			highlight = {
-				use_nvim_cmp_as_default = true,
-				winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+				ns = vim.api.nvim_create_namespace("blink_cmp"),
+				-- sets the fallback highlight groups to nvim-cmp's highlight groups
+				-- useful for when your theme doesn't support blink.cmp
+				-- will be removed in a future release, assuming themes add support
+				use_nvim_cmp_as_default = false,
 			},
 			nerd_font_variant = "mono",
 		},
