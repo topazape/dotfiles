850607fbc905ca2a82aaacec4ef5aff0bc97ff62
topazape <topazape@gmail.com>
2024-10-09 18:44:08 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -109,7 +109,7 @@ return {
 				-- sets the fallback highlight groups to nvim-cmp's highlight groups
 				-- useful for when your theme doesn't support blink.cmp
 				-- will be removed in a future release, assuming themes add support
-				use_nvim_cmp_as_default = false,
+				use_nvim_cmp_as_default = true,
 			},
 			nerd_font_variant = "mono",
 		},
