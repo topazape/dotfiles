7cddcaf29cf61842c7914d1ef347a3977f892835
topazape <topazape@gmail.com>
2024-10-12 10:03:38 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,13 +1,11 @@
 return {
 	{
-		"folke/tokyonight.nvim",
+		"Zeioth/neon.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require("tokyonight").setup({
-				style = "night",
-			})
-			vim.cmd.colorscheme("tokyonight")
+			require("neon").setup({})
+			vim.cmd.colorscheme("neon-cyberpunk-night")
 		end,
 	},
 
