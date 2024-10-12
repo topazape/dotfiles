cf6065688aca1d7476bc0f04bc9d799956cfb22d
topazape <topazape@gmail.com>
2024-10-12 10:04:02 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("neon").setup({})
-			vim.cmd.colorscheme("neon-cherrykiss-night")
+			vim.cmd.colorscheme("neon-netrunner-night")
 		end,
 	},
 
