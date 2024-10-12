7a52c31dc0272a324cb18db70eb4dc617d4d4cd9
topazape <topazape@gmail.com>
2024-10-12 09:03:24 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -6,7 +6,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("github-theme").setup({})
-			vim.cmd.colorscheme("github_dark")
+			vim.cmd.colorscheme("github_dark_default")
 		end,
 	},
 
