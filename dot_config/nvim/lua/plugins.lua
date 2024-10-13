41bbf31ca3a4b82af165c18489436d756f49b3c0
topazape <topazape@gmail.com>
2024-10-13 19:35:21 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     config = function()
       require("neomodern").setup({
-        style = "coffeecat",
+        style = "roseprime",
       })
       require("neomodern").load()
     end,
