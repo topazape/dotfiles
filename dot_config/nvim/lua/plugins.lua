aa359f7ab92214dbba1ef0c6b5cbf67189498202
topazape <topazape@gmail.com>
2024-10-13 11:56:08 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("everforest").setup({
-				style = "hard",
+				style = "hardness",
 			})
 			vim.cmd.colorscheme("everforest")
 		end,
