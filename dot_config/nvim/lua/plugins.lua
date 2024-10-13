932862bb3f06a96e41c654f37eb76127275ec7b9
topazape <topazape@gmail.com>
2024-10-13 19:40:38 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -3,9 +3,6 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "day"
-    },
     config = function()
       require("tokyonight").setup({
         style = "day",
