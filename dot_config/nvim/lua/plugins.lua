2b10ddbd1ebeac21d7122685492e26c6fc024941
topazape <topazape@gmail.com>
2024-10-13 19:39:58 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -3,7 +3,9 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      style = "night"
+    },
     config = function()
       vim.cmd.colorscheme("tokyonight")
     end
