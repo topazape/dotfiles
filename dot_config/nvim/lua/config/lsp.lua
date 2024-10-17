02af7284faf0c314bb37f40d1141e03a3785c7b4
topazape <topazape@gmail.com>
2024-10-17 17:30:58 +0900

    Update .config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -72,7 +72,7 @@ local handlers = {
 		require("lspconfig").basedpyright.setup({
 			settings = {
 				basedpyright = {
-					venvPath = vim.env.VIRTUAL_ENV or ".venv",
+					venvPath = ".venv",
 					analysis = {
 						typeCheckingMode = "recommended", -- off, basic, standard, strict, recommended, all
 						diagnosticMode = "workspace",
