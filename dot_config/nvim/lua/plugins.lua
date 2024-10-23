cde7876fadd591202ced4b4db350f0694915b44b
topazape <topazape@gmail.com>
2024-10-23 14:13:20 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -84,44 +84,48 @@ return {
 
 	-- completion
 	{
-		"saghen/blink.cmp",
-		lazy = false,
-		version = "v0.*",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
-		opts = {
-			keymap = {
-				accept = { "<C-f>", "<Tab>" },
-				scroll_documentation_up = "<C-k>",
-				scroll_documentation_down = "<C-j>",
-			},
-			trigger = {
-				completion = {
-					show_on_insert_on_trigger_character = false,
-				},
-				signature_help = {
-					enabled = true,
-				},
-			},
-			windows = {
-				autocomplete = {
-					border = "rounded",
-					selection = "manual",
-					draw = "reversed",
-				},
-				documentation = {
-					border = "rounded",
-					auto_show = true,
-				},
-				signature_help = {
-					border = "rounded",
-				},
-			},
-			highlight = {},
-			nerd_font_variant = "mono",
-		},
+		"max397574/care.nvim",
+		config = true,
 	},
+	-- {
+	-- 	"saghen/blink.cmp",
+	-- 	lazy = false,
+	-- 	version = "v0.*",
+	-- 	dependencies = {
+	-- 		"rafamadriz/friendly-snippets",
+	-- 	},
+	-- 	opts = {
+	-- 		keymap = {
+	-- 			accept = { "<C-f>", "<Tab>" },
+	-- 			scroll_documentation_up = "<C-k>",
+	-- 			scroll_documentation_down = "<C-j>",
+	-- 		},
+	-- 		trigger = {
+	-- 			completion = {
+	-- 				show_on_insert_on_trigger_character = false,
+	-- 			},
+	-- 			signature_help = {
+	-- 				enabled = true,
+	-- 			},
+	-- 		},
+	-- 		windows = {
+	-- 			autocomplete = {
+	-- 				border = "rounded",
+	-- 				selection = "manual",
+	-- 				draw = "reversed",
+	-- 			},
+	-- 			documentation = {
+	-- 				border = "rounded",
+	-- 				auto_show = true,
+	-- 			},
+	-- 			signature_help = {
+	-- 				border = "rounded",
+	-- 			},
+	-- 		},
+	-- 		highlight = {},
+	-- 		nerd_font_variant = "mono",
+	-- 	},
+	-- },
 
 	-- formatter
 	{
