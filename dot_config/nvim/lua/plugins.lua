0078c606036d99a4a14cc6907da720e687d13a64
topazape <topazape@gmail.com>
2024-10-26 00:12:11 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -277,9 +277,8 @@ return {
 				buftype = {
 					nofile = {
 						render_modes = { "n", "c", "i" },
-						code = {
-							width = "full",
-						},
+						code = { width = "full" },
+						debounce = 10,
 					},
 				},
 			},
