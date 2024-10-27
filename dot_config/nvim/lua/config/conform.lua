0ae5d479ae4017895a6823d13f9976d8cdabdcd6
topazape <topazape@gmail.com>
2024-10-28 08:46:54 +0900

    Update .config/nvim/lua/config/conform.lua

--- dot_config/nvim/lua/config/conform.lua
+++ dot_config/nvim/lua/config/conform.lua
@@ -1,55 +1,55 @@
 local conform = require("conform")
 
 conform.setup({
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
-  formatters_by_ft = {
-    css = {
-      "prettier",
-    },
-    bash = {
-      "shfmt",
-    },
-    go = {
-      "goimports",
-      "gofumpt",
-    },
-    html = {
-      "prettier",
-    },
-    javascript = {
-      "prettier",
-    },
-    json = {
-      "prettier",
-    },
-    lua = {
-      "stylua",
-    },
-    markdown = {
-      "prettier",
-    },
-    python = {
-      "ruff_organize_imports",
-      "ruff_format",
-    },
-    sh = {
-      "shfmt",
-    },
-    toml = {
-      "taplo",
-    },
-    yaml = {
-      "prettier",
-    },
-    zsh = {
-      "shfmt",
-    },
-    ["*"] = {
-      "trim_newlines",
-      "trim_whitespace",
-    },
-  },
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+	formatters_by_ft = {
+		css = {
+			"prettier",
+		},
+		bash = {
+			"shfmt",
+		},
+		go = {
+			"gofumpt",
+			"goimports",
+		},
+		html = {
+			"prettier",
+		},
+		javascript = {
+			"prettier",
+		},
+		json = {
+			"prettier",
+		},
+		lua = {
+			"stylua",
+		},
+		markdown = {
+			"prettier",
+		},
+		python = {
+			"ruff_organize_imports",
+			"ruff_format",
+		},
+		sh = {
+			"shfmt",
+		},
+		toml = {
+			"taplo",
+		},
+		yaml = {
+			"prettier",
+		},
+		zsh = {
+			"shfmt",
+		},
+		["*"] = {
+			"trim_newlines",
+			"trim_whitespace",
+		},
+	},
 })
