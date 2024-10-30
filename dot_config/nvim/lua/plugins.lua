aed2399960d640531a9fc9df4e86e7cc146d0247
topazape <topazape@gmail.com>
2024-10-30 10:01:07 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -82,224 +82,219 @@ return {
 		"j-hui/fidget.nvim",
 		config = true,
 	},
-	---- diagnostics
+
+	-- completion
 	{
-		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-		opts = { virtual_text = false },
+		"saghen/blink.cmp",
+		lazy = false,
+		version = "v0.*",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+		},
+		opts = {
+			keymap = {
+				accept = { "<C-f>", "<Tab>" },
+				scroll_documentation_up = "<C-k>",
+				scroll_documentation_down = "<C-j>",
+			},
+			trigger = {
+				signature_help = {
+					enabled = true,
+				},
+			},
+			windows = {
+				autocomplete = {
+					border = "rounded",
+					draw = "reversed",
+				},
+				documentation = {
+					border = "rounded",
+					auto_show = true,
+				},
+				signature_help = {
+					border = "rounded",
+				},
+			},
+			nerd_font_variant = "mono",
+		},
 	},
 
-	-- -- completion
-	-- {
-	-- 	"saghen/blink.cmp",
-	-- 	lazy = false,
-	-- 	version = "v0.*",
-	-- 	dependencies = {
-	-- 		"rafamadriz/friendly-snippets",
-	-- 	},
-	-- 	opts = {
-	-- 		keymap = {
-	-- 			accept = { "<C-f>", "<Tab>" },
-	-- 			scroll_documentation_up = "<C-k>",
-	-- 			scroll_documentation_down = "<C-j>",
-	-- 		},
-	-- 		trigger = {
-	-- 			signature_help = {
-	-- 				enabled = true,
-	-- 			},
-	-- 		},
-	-- 		windows = {
-	-- 			autocomplete = {
-	-- 				border = "rounded",
-	-- 				draw = "reversed",
-	-- 			},
-	-- 			documentation = {
-	-- 				border = "rounded",
-	-- 				auto_show = true,
-	-- 			},
-	-- 			signature_help = {
-	-- 				border = "rounded",
-	-- 			},
-	-- 		},
-	-- 		nerd_font_variant = "mono",
-	-- 	},
-	-- },
-	--
-	-- -- formatter
-	-- {
-	-- 	"stevearc/conform.nvim",
-	-- 	config = function()
-	-- 		require("config/conform")
-	-- 	end,
-	-- },
-	--
-	-- -- telescopes
-	-- {
-	-- 	"nvim-telescope/telescope.nvim",
-	-- 	branch = "0.1.x",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 	},
-	-- 	config = function()
-	-- 		require("config/telescope")
-	-- 	end,
-	-- },
-	-- ---- file browser
-	-- {
-	-- 	"nvim-telescope/telescope-file-browser.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-telescope/telescope.nvim",
-	-- 	},
-	-- },
-	--
-	-- -- UI
-	-- ---- notify
-	-- {
-	-- 	"rcarriga/nvim-notify",
-	-- },
-	--
-	-- ---- status line
-	-- {
-	-- 	"hoob3rt/lualine.nvim",
-	-- 	config = function()
-	-- 		require("config/lualine")
-	-- 	end,
-	-- },
-	--
-	-- -- mode highlight
-	-- {
-	-- 	"rasulomaroff/reactive.nvim",
-	-- 	opts = {
-	-- 		builtin = {
-	-- 			cursorline = true,
-	-- 			cursor = true,
-	-- 			modemsg = true,
-	-- 		},
-	-- 	},
-	-- },
-	-- ---- help
-	-- {
-	-- 	"OXY2DEV/helpview.nvim",
-	-- 	lazy = false,
-	-- 	dependencies = {
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 	},
-	-- },
-	--
-	-- -- emphasis indent
-	-- {
-	-- 	"shellRaining/hlchunk.nvim",
-	-- 	event = { "BufReadPre", "BufNewFile" },
-	-- 	opts = {
-	-- 		chunk = {
-	-- 			enable = true,
-	-- 			chars = {
-	-- 				right_arrow = "▷",
-	-- 			},
-	-- 		},
-	-- 		line_num = {
-	-- 			enable = true,
-	-- 		},
-	-- 	},
-	-- },
-	--
-	-- -- highlight-undo
-	-- {
-	-- 	"tzachar/highlight-undo.nvim",
-	-- 	config = true,
-	-- },
-	--
-	-- -- colorcode preview
-	-- {
-	-- 	"brenoprata10/nvim-highlight-colors",
-	-- 	opts = {
-	-- 		render = "virtual",
-	-- 		virtual_symbol = "",
-	-- 		virtual_symbol_prefix = " ",
-	-- 		virtual_symbol_position = "eol",
-	-- 	},
-	-- },
-	--
-	-- -- AI
-	-- ---- copilot
-	-- {
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	opts = {
-	-- 		panel = { enabled = false },
-	-- 		suggestion = {
-	-- 			enabled = true,
-	-- 			auto_trigger = true,
-	-- 			keymap = {
-	-- 				accept = "<M-f>",
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
-	--
-	-- -- git
-	-- ---- gitsigns
-	-- {
-	-- 	"lewis6991/gitsigns.nvim",
-	-- 	config = function()
-	-- 		require("config/gitsigns")
-	-- 	end,
-	-- },
-	-- ---- conflict
-	-- {
-	-- 	"akinsho/git-conflict.nvim",
-	-- 	version = "*",
-	-- 	config = true,
-	-- },
-	-- ---- GitHub
-	-- {
-	-- 	"pwntester/octo.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-telescope/telescope.nvim",
-	-- 		"nvim-tree/nvim-web-devicons",
-	-- 	},
-	-- 	opts = {
-	-- 		use_local_fs = true,
-	-- 		ssh_aliases = {
-	-- 			["github-tver"] = "github.com",
-	-- 		},
-	-- 		suppress_missing_scope = {
-	-- 			projects_v2 = true,
-	-- 		},
-	-- 	},
-	-- },
-	--
-	-- -- markdown
-	-- {
-	-- 	"MeanderingProgrammer/render-markdown.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 		"nvim-tree/nvim-web-devicons",
-	-- 	},
-	-- 	opts = {
-	-- 		heading = { border = true },
-	-- 		code = {
-	-- 			width = "block",
-	-- 			left_margin = 2,
-	-- 		},
-	-- 		-- for blink.cmp documentation and signature_help
-	-- 		overrides = {
-	-- 			buftype = {
-	-- 				nofile = {
-	-- 					render_modes = { "n", "c", "i" },
-	-- 					code = { width = "full" },
-	-- 				},
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
-	--
-	-- -- typst
-	-- {
-	-- 	"chomosuke/typst-preview.nvim",
-	-- 	ft = "typst", -- or ft = 'typst'
-	-- 	version = "1.*",
-	-- 	build = function()
-	-- 		require("typst-preview").update()
-	-- 	end,
-	-- },
+	-- formatter
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("config/conform")
+		end,
+	},
+
+	-- telescopes
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("config/telescope")
+		end,
+	},
+	---- file browser
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+
+	-- UI
+	---- notify
+	{
+		"rcarriga/nvim-notify",
+	},
+
+	---- status line
+	{
+		"hoob3rt/lualine.nvim",
+		config = function()
+			require("config/lualine")
+		end,
+	},
+
+	-- mode highlight
+	{
+		"rasulomaroff/reactive.nvim",
+		opts = {
+			builtin = {
+				cursorline = true,
+				cursor = true,
+				modemsg = true,
+			},
+		},
+	},
+	---- help
+	{
+		"OXY2DEV/helpview.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
+
+	-- emphasis indent
+	{
+		"shellRaining/hlchunk.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			chunk = {
+				enable = true,
+				chars = {
+					right_arrow = "▷",
+				},
+			},
+			line_num = {
+				enable = true,
+			},
+		},
+	},
+
+	-- highlight-undo
+	{
+		"tzachar/highlight-undo.nvim",
+		config = true,
+	},
+
+	-- colorcode preview
+	{
+		"brenoprata10/nvim-highlight-colors",
+		opts = {
+			render = "virtual",
+			virtual_symbol = "",
+			virtual_symbol_prefix = " ",
+			virtual_symbol_position = "eol",
+		},
+	},
+
+	-- AI
+	---- copilot
+	{
+		"zbirenbaum/copilot.lua",
+		opts = {
+			panel = { enabled = false },
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				keymap = {
+					accept = "<M-f>",
+				},
+			},
+		},
+	},
+
+	-- git
+	---- gitsigns
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("config/gitsigns")
+		end,
+	},
+	---- conflict
+	{
+		"akinsho/git-conflict.nvim",
+		version = "*",
+		config = true,
+	},
+	---- GitHub
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			use_local_fs = true,
+			ssh_aliases = {
+				["github-tver"] = "github.com",
+			},
+			suppress_missing_scope = {
+				projects_v2 = true,
+			},
+		},
+	},
+
+	-- markdown
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			heading = { border = true },
+			code = {
+				width = "block",
+				left_margin = 2,
+			},
+			-- for blink.cmp documentation and signature_help
+			overrides = {
+				buftype = {
+					nofile = {
+						render_modes = { "n", "c", "i" },
+						code = { width = "full" },
+					},
+				},
+			},
+		},
+	},
+
+	-- typst
+	{
+		"chomosuke/typst-preview.nvim",
+		ft = "typst", -- or ft = 'typst'
+		version = "1.*",
+		build = function()
+			require("typst-preview").update()
+		end,
+	},
 }
