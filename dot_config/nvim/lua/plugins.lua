ab8c97e8a1203de7eebcb8006ba38c5589ddc542
topazape <topazape@gmail.com>
2024-10-30 10:13:52 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -82,6 +82,16 @@ return {
 		"j-hui/fidget.nvim",
 		config = true,
 	},
+	---- diagnostics display
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		config = function()
+			vim.diagnostic.config({
+				virtual_text = false,
+			})
+			require("lsp_lines").setup()
+		end,
+	},
 
 	-- completion
 	{
