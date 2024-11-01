22d640b0b6fce78cd4336ab8e2b923ff0043644e
topazape <topazape@gmail.com>
2024-11-01 10:20:46 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -93,7 +93,7 @@ return {
 		},
 		opts = {
 			keymap = {
-				["<C-g>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-;>"] = { "show", "show_documentation", "hide_documentation" },
 				["<C-f>"] = { "accept" },
 				["<TAB>"] = { "accept" },
 				["<C-p>"] = { "select_prev", "fallback" },
