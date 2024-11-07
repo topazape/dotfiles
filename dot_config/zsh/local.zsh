999d6dff75b9b9194055f0a64498087ea744571e
topazape <topazape@gmail.com>
2024-11-08 00:07:22 +0900

    Update .config/zsh/local.zsh

--- dot_config/zsh/local.zsh
+++ dot_config/zsh/local.zsh
@@ -112,6 +112,10 @@ if [[ -d $(brew --prefix)/share/google-cloud-sdk ]]; then
   source $(brew --prefix)/share/google-cloud-sdk/path.zsh.inc
   source $(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc
 fi
+## tenv
+if [[ -d $(brew --prefix tenv) ]]; then
+  export TENV_ROOT=${XDG_DATA_HOME}/tenv
+fi
 
 # Programing Languages
 ## LLVM
