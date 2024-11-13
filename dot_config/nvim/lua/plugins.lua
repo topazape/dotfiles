f2e158cb69a444662337022c2036ce6df3a0daf6
topazape <topazape@gmail.com>
2024-11-13 12:45:16 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -83,47 +83,47 @@ return {
 		config = true,
 	},
 
-	-- -- completion
-	-- {
-	-- 	"saghen/blink.cmp",
-	-- 	lazy = false,
-	-- 	version = "v0.*",
-	-- 	dependencies = {
-	-- 		"rafamadriz/friendly-snippets",
-	-- 	},
-	-- 	opts = {
-	-- 		keymap = {
-	-- 			["<C-g>"] = { "show", "show_documentation", "hide_documentation" },
-	-- 			["<C-f>"] = { "accept" },
-	-- 			["<TAB>"] = { "accept" },
-	-- 			["<C-p>"] = { "select_prev", "fallback" },
-	-- 			["<C-n>"] = { "select_next", "fallback" },
-	-- 			["<C-j>"] = { "scroll_documentation_down" },
-	-- 			["<C-k>"] = { "scroll_documentation_up" },
-	-- 		},
-	-- 		trigger = {
-	-- 			signature_help = {
-	-- 				enabled = true,
-	-- 			},
-	-- 		},
-	-- 		windows = {
-	-- 			autocomplete = {
-	-- 				border = "rounded",
-	-- 				selection = "auto_insert",
-	-- 				draw = "reversed",
-	-- 			},
-	-- 			documentation = {
-	-- 				border = "rounded",
-	-- 				auto_show = true,
-	-- 				auto_show_delay_ms = 100,
-	-- 			},
-	-- 			signature_help = {
-	-- 				border = "rounded",
-	-- 			},
-	-- 		},
-	-- 		nerd_font_variant = "mono",
-	-- 	},
-	-- },
+	-- completion
+	{
+		"saghen/blink.cmp",
+		lazy = false,
+		version = "v0.*",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+		},
+		opts = {
+			keymap = {
+				["<C-g>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-f>"] = { "accept" },
+				["<TAB>"] = { "accept" },
+				["<C-p>"] = { "select_prev", "fallback" },
+				["<C-n>"] = { "select_next", "fallback" },
+				["<C-j>"] = { "scroll_documentation_down" },
+				["<C-k>"] = { "scroll_documentation_up" },
+			},
+			trigger = {
+				signature_help = {
+					enabled = true,
+				},
+			},
+			windows = {
+				autocomplete = {
+					border = "rounded",
+					selection = "auto_insert",
+					draw = "reversed",
+				},
+				documentation = {
+					border = "rounded",
+					auto_show = true,
+					auto_show_delay_ms = 100,
+				},
+				signature_help = {
+					border = "rounded",
+				},
+			},
+			nerd_font_variant = "mono",
+		},
+	},
 
 	-- formatter
 	{
