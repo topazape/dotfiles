e15aaf8ad4194dff77bcecb8a68d5a74b3632183
topazape <topazape@gmail.com>
2024-12-03 10:25:16 +0900

    Update .config/nvim/lua/autocmds.lua

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -127,9 +127,9 @@ autocmd({ "FileType" }, {
 	pattern = { "xml" },
 	callback = function()
 		-- タグの折りたたみ
-		vim.opt_local.foldmethod = "indent"
+		-- vim.opt_local.foldmethod = "indent"
 		vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
-		vim.opt.fillchars = { fold = " " }
+		-- vim.opt.fillchars = { fold = " " }
 	end,
 })
 
