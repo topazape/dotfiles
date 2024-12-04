655a90d2f683376cfc4ab33a6a83f9f1ffaf1886
topazape <topazape@gmail.com>
2024-12-04 09:55:46 +0900

    Update .config/nvim/lua/autocmds.lua

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -24,6 +24,15 @@ autocmd({ "FileType" }, {
 		vim.opt_local.expandtab = false
 	end,
 })
+---- html
+autocmd({ "FileType" }, {
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.expandtab = true
+	end,
+})
 ---- lua
 autocmd({ "FileType" }, {
 	pattern = { "lua" },
