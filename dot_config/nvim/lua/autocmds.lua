9224809a10037f1f965c1384ec69fd5b5cca2dea
topazape <topazape@gmail.com>
2024-12-05 22:37:19 +0900

    Update .config/nvim/lua/autocmds.lua

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -137,6 +137,10 @@ autocmd({ "FileType" }, {
 	pattern = { "xml" },
 	callback = function()
 		vim.opt_local.foldmethod = "indent"
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.expandtab = true
 	end,
 })
 
