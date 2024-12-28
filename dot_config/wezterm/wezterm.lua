a9bb420df32d5131afc95ee2ac0535a1bf0e2602
topazape <topazape@gmail.com>
2024-12-28 23:13:47 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -30,6 +30,7 @@ return {
 	line_height = 1.10,
 
 	-- look and feel
+	windw_decorations = "RESIZE",
 	window_background_opacity = 0.98,
 	macos_window_background_blur = 20,
 	use_fancy_tab_bar = false,
