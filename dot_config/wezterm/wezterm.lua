e52d03eb900cbb1e2f299ce81f1bf59f937df745
topazape <topazape@gmail.com>
2025-01-04 21:47:37 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local DEFAULT_BG = "#1a1b26"
-local DEFAULT_ACTIVE_FG = "#7dcfff"
-local DEFAULT_INACTIVE_FG = "#3d59a1"
+local DEFAULT_ACTIVE_FG = "#3d59a1"
+local DEFAULT_INACTIVE_FG = "#394b70"
 local FONT_SIZE = 15
 local CONSOLE_FONTS = {
 	{ family = "TX-02" },
