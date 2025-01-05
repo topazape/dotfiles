a76a54e41431b96d9312d3a8faa55df5be6387b4
topazape <topazape@gmail.com>
2025-01-05 12:23:25 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,16 +1,24 @@
 return {
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("tokyonight").setup({
+	-- 			style = "night",
+	-- 		})
+	-- 		vim.cmd.colorscheme("tokyonight")
+	-- 		vim.cmd("highlight WinSeparator guifg=#3d59a1 guibg=None")
+	-- 		vim.cmd("highlight DiagnosticHint guifg=#ffc777 guibg=None")
+	-- 	end,
+	-- },
 	{
-		"folke/tokyonight.nvim",
+		"maxmx03/fluoromachine.nvim",
 		lazy = false,
 		priority = 1000,
-		config = function()
-			require("tokyonight").setup({
-				style = "night",
-			})
-			vim.cmd.colorscheme("tokyonight")
-			vim.cmd("highlight WinSeparator guifg=#3d59a1 guibg=None")
-			vim.cmd("highlight DiagnosticHint guifg=#ffc777 guibg=None")
-		end,
+		opts = {
+			theme = "fluoromachine",
+		},
 	},
 
 	-- devicon
