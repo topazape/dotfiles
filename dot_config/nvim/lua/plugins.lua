e5137ba51a194c0d6a1154f7caa86800e3199025
topazape <topazape@gmail.com>
2025-01-09 14:43:51 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -92,66 +92,9 @@ return {
 			"rafamadriz/friendly-snippets",
 			"giuxtaposition/blink-cmp-copilot",
 		},
-		opts = {
-			keymap = {
-				preset = "none",
-
-				["<C-g>"] = { "show", "show_documentation", "hide_documentation" },
-				["<C-f>"] = { "accept", "fallback" },
-				["<CR>"] = { "accept", "fallback" },
-				["<C-n>"] = { "select_next", "fallback" },
-				["<C-p>"] = { "select_prev", "fallback" },
-				["<C-j>"] = { "scroll_documentation_down", "fallback" },
-				["<C-k>"] = { "scroll_documentation_up", "fallback" },
-
-				cmdline = {
-					["<TAB>"] = { "select_next", "fallback" },
-					["<S-TAB>"] = { "select_prev", "fallback" },
-					["<C-n>"] = { "select_next", "fallback" },
-					["<C-p>"] = { "select_prev", "fallback" },
-				},
-			},
-			completion = {
-				menu = {
-					enabled = true,
-					border = "rounded",
-					draw = {
-						columns = {
-							{ "label", "label_description", gap = 1 },
-							{ "kind_icon", "kind" },
-						},
-					},
-				},
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 50,
-					window = {
-						border = "rounded",
-					},
-				},
-				ghost_text = {
-					enabled = false,
-				},
-			},
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer", "copilot" },
-				providers = {
-					copilot = {
-						name = "copilot",
-						module = "blink-cmp-copilot",
-						score_offset = 100,
-						async = true,
-					},
-				},
-				cmdline = {},
-			},
-			signature = {
-				enabled = true,
-				window = {
-					border = "rounded",
-				},
-			},
-		},
+		config = function()
+			require("config/blink-cmp")
+		end,
 	},
 
 	-- formatter
