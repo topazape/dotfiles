84d4c7fe560f7e102d6dfe9696877137c75ad2fc
topazape <topazape@gmail.com>
2025-01-10 14:21:37 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -23,8 +23,7 @@ return {
 	front_end = "OpenGL",
 
 	-- color scheme
-	-- color_scheme = "tokyonight_night",
-	color_scheme = "Dracula+",
+	color_scheme = "tokyonight_night",
 
 	-- colors
 	colors = {
