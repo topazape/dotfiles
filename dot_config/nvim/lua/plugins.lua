fbb1f4a057b6c90998406af6cf4377ae002889a2
topazape <topazape@gmail.com>
2025-01-10 13:27:07 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,22 +1,15 @@
 return {
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("tokyonight").setup({
-	-- 			style = "night",
-	-- 		})
-	-- 		vim.cmd.colorscheme("tokyonight")
-	-- 		vim.cmd("highlight WinSeparator guifg=#3d59a1 guibg=None")
-	-- 		vim.cmd("highlight DiagnosticHint guifg=#ffc777 guibg=None")
-	-- 	end,
-	-- },
 	{
-		"kepano/flexoki-neovim",
-		name = "flexoki",
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("flexoki-dark")
+			require("tokyonight").setup({
+				style = "night",
+			})
+			vim.cmd.colorscheme("tokyonight")
+			vim.cmd("highlight WinSeparator guifg=#3d59a1 guibg=None")
+			vim.cmd("highlight DiagnosticHint guifg=#ffc777 guibg=None")
 		end,
 	},
 
