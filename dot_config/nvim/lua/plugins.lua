4a891ffbedff118276fddb01cd5bfdf2054beaa9
topazape <topazape@gmail.com>
2025-01-10 13:29:40 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,17 +1,30 @@
 return {
 	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
+		"ray-x/aurora",
+		init = function()
+			vim.g.aurora_italic = 1
+			vim.g.aurora_transparent = 1
+			vim.g.aurora_bold = 1
+		end,
 		config = function()
-			require("tokyonight").setup({
-				style = "night",
-			})
-			vim.cmd.colorscheme("tokyonight")
-			vim.cmd("highlight WinSeparator guifg=#3d59a1 guibg=None")
-			vim.cmd("highlight DiagnosticHint guifg=#ffc777 guibg=None")
+			vim.cmd.colorscheme("aurora")
+			-- override defaults
+			vim.api.nvim_set_hl(0, "@number", { fg = "#e933e3" })
 		end,
 	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("tokyonight").setup({
+	-- 			style = "night",
+	-- 		})
+	-- 		vim.cmd.colorscheme("tokyonight")
+	-- 		vim.cmd("highlight WinSeparator guifg=#3d59a1 guibg=None")
+	-- 		vim.cmd("highlight DiagnosticHint guifg=#ffc777 guibg=None")
+	-- 	end,
+	-- },
 
 	-- devicon
 	{
