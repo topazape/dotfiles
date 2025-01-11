c45e9db9ac5db77940eb4f4a6c9483bad3f32fff
topazape <topazape@gmail.com>
2025-01-11 21:41:28 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -85,7 +85,7 @@ return {
 	---- inline diagnostics
 	{
 		"rachartier/tiny-inline-diagnostic.nvim",
-		event = "VeryLazy",
+		event = "LspAttach",
 		priority = 1000,
 		config = function()
 			require("tiny-inline-diagnostic").setup({})
