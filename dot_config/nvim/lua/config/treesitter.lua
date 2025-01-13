db29bf2939bb276a13678f8f81ec9d2eef38a872
topazape <topazape@gmail.com>
2025-01-13 23:38:35 +0900

    Update .config/nvim/lua/config/treesitter.lua

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -5,7 +5,7 @@ require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true,
 		disable = {
-			"lua",
+			-- "lua",
 		},
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
