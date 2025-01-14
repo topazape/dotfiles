a36b20de60557ddef74c45c43d00c9ac7055fdb6
topazape <topazape@gmail.com>
2025-01-14 21:34:09 +0900

    Update .config/nvim/lua/autocmds.lua

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -138,6 +138,7 @@ autocmd({ "FileType" }, {
 	callback = function()
 		vim.opt_local.foldmethod = "expr"
 		vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.opt_local.foldlevel = 2
 
 		vim.opt_local.tabstop = 2
 		vim.opt_local.shiftwidth = 2
