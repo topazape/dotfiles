7222c5815b0816d370dd5e860e5488de753f535a
topazape <topazape@gmail.com>
2025-01-17 10:46:35 +0900

    Update .config/nvim/lua/config/blink-cmp.lua

--- dot_config/nvim/lua/config/blink-cmp.lua
+++ dot_config/nvim/lua/config/blink-cmp.lua
@@ -46,6 +46,14 @@ blinkcmp.setup({
 		},
 	},
 	sources = {
+		default = { "lsp", "path", "snippets", "buffer", "markdown" },
+		providers = {
+			markdown = {
+				name = "RenderMarkdown",
+				module = "render-markdown.integ.blink",
+				fallbacks = { "lsp" },
+			},
+		},
 		cmdline = {},
 	},
 	signature = {
