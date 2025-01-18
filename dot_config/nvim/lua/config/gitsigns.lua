fd6f68a173e5c0c85b1b07c44898cb9d214d09db
topazape <topazape@gmail.com>
2025-01-18 17:31:08 +0900

    Update .config/nvim/lua/config/gitsigns.lua

--- dot_config/nvim/lua/config/gitsigns.lua
+++ dot_config/nvim/lua/config/gitsigns.lua
@@ -3,7 +3,7 @@ gitsigns.setup({
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
-	word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	current_line_blame = false,
 	current_line_blame_opts = {
 		delay = 100,
