1ba599d5d303e1dc530b37e0ca3b45b04934ad9b
topazape <topazape@gmail.com>
2025-01-27 09:36:54 +0900

    Update .config/nvim/lua/options.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -85,17 +85,3 @@ local options = {
 for k, v in pairs(options) do
 	vim.opt[k] = v
 end
-
--- Diagnostic signs
-local diagnostic_signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(diagnostic_signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-end
-
-vim.diagnostic.config({ virtual_lines = true })
