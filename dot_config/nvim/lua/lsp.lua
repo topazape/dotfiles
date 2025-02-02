c0b7fe064ecfefd322c1d3989698c30bb8bb5f61
topazape <topazape@gmail.com>
2025-02-02 21:13:15 +0900

    Update .config/nvim/lua/lsp.lua

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -2,7 +2,7 @@ vim.diagnostic.config({
 	severity_sort = true,
 	signs = {
 		text = {
-			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.ERROR] = "⁕",
 			[vim.diagnostic.severity.WARN] = "",
 			[vim.diagnostic.severity.INFO] = "",
 			[vim.diagnostic.severity.HINT] = "",
