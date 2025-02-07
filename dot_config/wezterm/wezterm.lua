5b73e120a75f97f5f7dac152977559227c50bce8
topazape <topazape@gmail.com>
2025-02-07 17:12:07 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local DEFAULT_INACTIVE_FG = "#3a3a3e"
 local DEFAULT_BG = "#121213"
 local FONT_SIZE = 15
 local CONSOLE_FONTS = {
-	{ family = "Brass Mono" },
+	{ family = "Brass Mono Code" },
 	{ family = "TX-02" },
 	{ family = "IBM Plex Sans JP" },
 }
