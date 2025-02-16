ae3781a09de62f2d0cce099d6c0bff3d824758b6
topazape <topazape@gmail.com>
2025-02-16 09:52:53 +0900

    Update .config/nvim/lua/config/blink-cmp.lua

--- dot_config/nvim/lua/config/blink-cmp.lua
+++ dot_config/nvim/lua/config/blink-cmp.lua
@@ -14,8 +14,6 @@ blinkcmp.setup({
 
 	cmdline = {
 		keymap = {
-			preset = "none",
-
 			["<TAB>"] = { "select_next", "fallback" },
 			["<S-TAB>"] = { "select_prev", "fallback" },
 			["<C-n>"] = { "select_next", "fallback" },
