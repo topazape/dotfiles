93ecd972c28fd7301a8571d4f04510beae0d3103
topazape <topazape@gmail.com>
2025-03-01 10:58:27 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -187,31 +187,31 @@ return {
 		},
 	},
 	---- avante
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		lazy = false,
-		version = false,
-		build = "make",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"zbirenbaum/copilot.lua",
-		},
-		opts = {
-			provider = "copilot",
-			auto_suggestions_provider = "copilot",
-			behaviour = {
-				auto_suggestions = true,
-				auto_set_highlight_group = true,
-				auto_set_keymaps = true,
-				auto_apply_diff_after_generation = true,
-				support_paste_from_clipboard = true,
-			},
-		},
-	},
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	event = "VeryLazy",
+	-- 	lazy = false,
+	-- 	version = false,
+	-- 	build = "make",
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"stevearc/dressing.nvim",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"zbirenbaum/copilot.lua",
+	-- 	},
+	-- 	opts = {
+	-- 		provider = "copilot",
+	-- 		auto_suggestions_provider = "copilot",
+	-- 		behaviour = {
+	-- 			auto_suggestions = true,
+	-- 			auto_set_highlight_group = true,
+	-- 			auto_set_keymaps = true,
+	-- 			auto_apply_diff_after_generation = true,
+	-- 			support_paste_from_clipboard = true,
+	-- 		},
+	-- 	},
+	-- },
 
 	-- git
 	---- gitsigns
