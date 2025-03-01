2f761837b43711a78ba3ce87b257f5adc73e92a1
topazape <topazape@gmail.com>
2025-03-01 18:49:01 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -204,7 +204,7 @@ return {
 			provider = "copilot",
 			auto_suggestions_provider = "copilot",
 			copilot = {
-				model = "claude-3.5-sonnet",
+				model = "claude-3-5-sonnet",
 			},
 			behaviour = {
 				auto_suggestions = true,
