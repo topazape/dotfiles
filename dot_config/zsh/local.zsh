938c59883488a4639e7d8d33bee99c9e3a5f7f6d
topazape <topazape@gmail.com>
2025-03-05 22:26:02 +0900

    Update .config/zsh/local.zsh

--- dot_config/zsh/local.zsh
+++ dot_config/zsh/local.zsh
@@ -85,7 +85,7 @@ fi
 ## gtrash (https://github.com/umlx5h/gtrash)
 if type gtrash >/dev/null 2>&1; then
   eval "$(gtrash completion zsh)"
-  alias gm="gtrash put"
+  alias tm="gtrash put"
   alias rm="echo -e 'If you want to use rm really, then use \"\\\\rm\" instead.'; false"
 fi
 ## bat
