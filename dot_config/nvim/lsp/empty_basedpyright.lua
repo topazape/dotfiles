5bbd890e4598524b19291698ed159ceeab09c941
topazape <topazape@gmail.com>
2025-03-28 13:03:17 +0900

    Update .config/nvim/lsp/basedpyright.lua

--- dot_config/nvim/lsp/empty_basedpyright.lua
+++ dot_config/nvim/lsp/empty_basedpyright.lua
@@ -9,31 +9,31 @@ return {
 		"setup.cfg",
 		"setup.py",
 	},
-	settings = {
-		python = {
-			pythonPath = "./.venv/bin/python",
-		},
-		basedpyright = {
-			analysis = {
-				typeCheckingMode = "recommended", -- off, basic, standard, strict, recommended, all
-				diagnosticMode = "workspace",
-				diagnosticSeverityOverrides = {
-					reportAny = false,
-					reportExplicitAny = false,
-					reportUnusedImport = false,
-					reportUnusedVariable = false,
-					reportAttributeAccessIssue = false,
-					reportUnknownArgumentType = false,
-					reportUnknownMemberType = false,
-					reportUnknownVariableType = false,
-				},
-				inlayHints = {
-					callArgumentNames = false,
-					functionReturnTypes = false,
-					genericTypes = true,
-					variableTypes = true,
-				},
-			},
-		},
-	},
+	-- settings = {
+	-- 	python = {
+	-- 		pythonPath = "./.venv/bin/python",
+	-- 	},
+	-- 	basedpyright = {
+	-- 		analysis = {
+	-- 			typeCheckingMode = "recommended", -- off, basic, standard, strict, recommended, all
+	-- 			diagnosticMode = "workspace",
+	-- 			diagnosticSeverityOverrides = {
+	-- 				reportAny = false,
+	-- 				reportExplicitAny = false,
+	-- 				reportUnusedImport = false,
+	-- 				reportUnusedVariable = false,
+	-- 				reportAttributeAccessIssue = false,
+	-- 				reportUnknownArgumentType = false,
+	-- 				reportUnknownMemberType = false,
+	-- 				reportUnknownVariableType = false,
+	-- 			},
+	-- 			inlayHints = {
+	-- 				callArgumentNames = false,
+	-- 				functionReturnTypes = false,
+	-- 				genericTypes = true,
+	-- 				variableTypes = true,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
