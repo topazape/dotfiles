6d3802abd84d017f0f2db893751e90e8a516d346
topazape <topazape@gmail.com>
2025-03-28 12:59:59 +0900

    Update .config/nvim/lsp/basedpyright.lua

---
+++ dot_config/nvim/lsp/empty_basedpyright.lua
@@ -0,0 +1,38 @@
+return {
+	root_markers = {
+		"Pipfile",
+		"pyproject.toml",
+		"pyrightconfig.json",
+		"requirements.txt",
+		"setup.cfg",
+		"setup.py",
+	},
+
+	settings = {
+		-- python = {
+		-- 	pythonPath = "./.venv/bin/python",
+		-- },
+		basedpyright = {
+			analysis = {
+				typeCheckingMode = "recommended", -- off, basic, standard, strict, recommended, all
+				diagnosticMode = "workspace",
+				diagnosticSeverityOverrides = {
+					reportAny = false,
+					reportExplicitAny = false,
+					reportUnusedImport = false,
+					reportUnusedVariable = false,
+					reportAttributeAccessIssue = false,
+					reportUnknownArgumentType = false,
+					reportUnknownMemberType = false,
+					reportUnknownVariableType = false,
+				},
+				inlayHints = {
+					callArgumentNames = false,
+					functionReturnTypes = false,
+					genericTypes = true,
+					variableTypes = true,
+				},
+			},
+		},
+	},
+}
