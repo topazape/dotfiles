5ed606d2e2bd31f1c5fbe8179b0952643736db9b
topazape <topazape@gmail.com>
2025-03-28 13:08:53 +0900

    Remove .config/nvim/lsp/gopls.lua Add .config/nvim/lsp/gopls.lua

---
+++ dot_config/nvim/lsp/gopls.lua
@@ -0,0 +1,22 @@
+return {
+	cmd = { "gopls" },
+	cmd_env = { GOPLSCACHE = vim.env.XDG_CACHE_HOME .. "/gopls--" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_markers = { "go.work", "go.mod" },
+	settings = {
+		gopls = {
+			analyses = {
+				fillstruct = true,
+			},
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = false,
+				rangeVariableTypes = true,
+			},
+		},
+	},
+}
