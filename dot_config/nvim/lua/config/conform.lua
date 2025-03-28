b1ece64624a23bbd295d30ab50c512862b946f6e
topazape <topazape@gmail.com>
2025-03-28 23:49:19 +0900

    Update .config/nvim/lua/config/conform.lua

--- dot_config/nvim/lua/config/conform.lua
+++ dot_config/nvim/lua/config/conform.lua
@@ -10,7 +10,7 @@ conform.setup({
 			command = "sqlfluff",
 			args = { "format", "-" },
 			cwd = function()
-				return require("conform.util").root_file({ ".git", ".sqlfluff" })
+				return tostring(require("conform.util").root_file({ ".git", ".sqlfluff" }))
 			end,
 		},
 	},
