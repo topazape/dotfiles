efcdfd11d091d15313d4dce0373eb6fb637f034d
topazape <topazape@gmail.com>
2025-03-28 13:04:30 +0900

    Update .config/nvim/lsp/basedpyright.lua

--- dot_config/nvim/lsp/empty_basedpyright.lua
+++ dot_config/nvim/lsp/empty_basedpyright.lua
@@ -1,6 +1,6 @@
 return {
-  cmd = { 'basedpyright-langserver', '--stdio' },
-  filetypes = { 'python' },
+	cmd = { "basedpyright-langserver", "--stdio" },
+	filetypes = { "python" },
 	root_markers = {
 		"Pipfile",
 		"pyproject.toml",
@@ -9,31 +9,31 @@ return {
 		"setup.cfg",
 		"setup.py",
 	},
-	-- settings = {
-	-- 	python = {
-	-- 		pythonPath = "./.venv/bin/python",
-	-- 	},
-	-- 	basedpyright = {
-	-- 		analysis = {
-	-- 			typeCheckingMode = "recommended", -- off, basic, standard, strict, recommended, all
-	-- 			diagnosticMode = "workspace",
-	-- 			diagnosticSeverityOverrides = {
-	-- 				reportAny = false,
-	-- 				reportExplicitAny = false,
-	-- 				reportUnusedImport = false,
-	-- 				reportUnusedVariable = false,
-	-- 				reportAttributeAccessIssue = false,
-	-- 				reportUnknownArgumentType = false,
-	-- 				reportUnknownMemberType = false,
-	-- 				reportUnknownVariableType = false,
-	-- 			},
-	-- 			inlayHints = {
-	-- 				callArgumentNames = false,
-	-- 				functionReturnTypes = false,
-	-- 				genericTypes = true,
-	-- 				variableTypes = true,
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
+	settings = {
+		-- python = {
+		-- 	pythonPath = "./.venv/bin/python",
+		-- },
+		basedpyright = {
+			analysis = {
+				typeCheckingMode = "recommended", -- off, basic, standard, strict, recommended, all
+				diagnosticMode = "workspace",
+				diagnosticSeverityOverrides = {
+					reportAny = false,
+					reportExplicitAny = false,
+					reportUnusedImport = false,
+					reportUnusedVariable = false,
+					reportAttributeAccessIssue = false,
+					reportUnknownArgumentType = false,
+					reportUnknownMemberType = false,
+					reportUnknownVariableType = false,
+				},
+				inlayHints = {
+					callArgumentNames = false,
+					functionReturnTypes = false,
+					genericTypes = true,
+					variableTypes = true,
+				},
+			},
+		},
+	},
 }
