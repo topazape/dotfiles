7f578a54b15bc9526d8b500a7df90123ee720353
topazape <topazape@gmail.com>
2025-03-28 11:10:16 +0900

    Update .config/nvim/lua/lsp.lua

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -28,3 +28,18 @@ vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
 	:each(function(server_name)
 		vim.lsp.enable(server_name)
 	end)
+---- enable inlay hints
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local clienst = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		-- skip proxy LSPs
+		local proxy_lsps = { "copilot" }
+		local is_proxy = false
+		for _, name in ipairs(proxy_lsps) do
+			if client.name == name then
+				is_proxy = true
+				break
+			end
+		end
+	end,
+})
