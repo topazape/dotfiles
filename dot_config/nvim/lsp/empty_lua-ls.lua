29189ea182ac2144adb0421a565eafce3865b5c7
topazape <topazape@gmail.com>
2025-03-28 13:56:49 +0900

    Update .config/nvim/lsp/lua-ls.lua

---
+++ dot_config/nvim/lsp/empty_lua-ls.lua
@@ -0,0 +1,81 @@
+return {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = {
+		".luarc.json",
+		".luarc.jsonc",
+		".luacheckrc",
+		".stylua.toml",
+		"stylua.toml",
+		"selene.toml",
+		"selene.yml",
+	},
+	settings = {
+		Lua = {
+			codeLens = {
+				enable = true,
+			},
+			completion = {
+				autoRequire = false,
+				callSnippet = "Replace",
+				keywordSnippet = "Both",
+				workspaceWord = true,
+			},
+			diagnostics = {
+				disable = {
+					"inject-field",
+					"missing-fields",
+					"missing-parameter",
+				},
+				globals = {
+					"LazyVim",
+					"Snacks",
+					"bit",
+					"colors",
+					"defaults",
+					"describe",
+					"ev",
+					"hl",
+					"it",
+					"keys",
+					"math",
+					"ns",
+					"package",
+					"require",
+					"vim",
+				},
+				unusedLocalExclude = {
+					"_*",
+				},
+			},
+			doc = {
+				privateName = { "^_" },
+			},
+			format = {
+				enable = false,
+			},
+			hint = {
+				arrayIndex = "Disable",
+				enable = true,
+				paramName = "Disable",
+				paramType = true,
+				semicolon = "Disable",
+				setType = true,
+			},
+			hover = {
+				expandAlias = false,
+			},
+			runtime = {
+				version = "LuaJIT",
+			},
+			telemetry = {
+				enable = false,
+			},
+			type = {
+				castNumberToInteger = true,
+				inferParamType = true,
+				checkTableShape = true,
+			},
+		},
+	},
+}
