40e88dc688c4710203d568f09d6863c488b7ecb6
topazape <topazape@gmail.com>
2025-03-28 13:02:22 +0900

    Update .config/nvim/lsp/basedpyright.lua

--- dot_config/nvim/lsp/empty_basedpyright.lua
+++ dot_config/nvim/lsp/empty_basedpyright.lua
@@ -1,6 +1,6 @@
 return {
-  cmd = { 'basedpyright-langserver', '--stdio' },
-  filetypes = { 'python' },
+  -- cmd = { 'basedpyright-langserver', '--stdio' },
+  -- filetypes = { 'python' },
 	root_markers = {
 		"Pipfile",
 		"pyproject.toml",
