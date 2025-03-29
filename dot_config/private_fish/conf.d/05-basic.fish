5f55d02e938796f437d8d2752448b078c5f99736
topazape <topazape@gmail.com>
2025-03-29 19:24:57 +0900

    Update .config/fish/conf.d/05-basic.fish

--- dot_config/private_fish/conf.d/05-basic.fish
+++ dot_config/private_fish/conf.d/05-basic.fish
@@ -1,5 +1,5 @@
 # less
 if type -q less
-  set -gx LESSHISTFILE -
-  set -gx LESS '-gj10 --no-init --quit-if-one-screen --RAW-CONTROL-CHARS'
+    set -gx LESSHISTFILE -
+    set -gx LESS '-gj10 --no-init --quit-if-one-screen --RAW-CONTROL-CHARS'
 end
