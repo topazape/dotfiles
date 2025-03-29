1bc672848ca4dd277f2ae6caea9b91e4862610a8
topazape <topazape@gmail.com>
2025-03-29 22:08:35 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local DEFAULT_INACTIVE_FG = "#3a3a3e"
 local DEFAULT_BG = "#121213"
 local FONT_SIZE = 16
 local CONSOLE_FONTS = {
-	{ family = "Maple Mono" },
 	{ family = "TX-02" },
+	{ family = "Maple Mono" },
 	{ family = "Pretendard JP" },
 }
 
