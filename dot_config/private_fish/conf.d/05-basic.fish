7e290bdaf0d65473df22f742473a36041d06ed8e
topazape <topazape@gmail.com>
2025-03-29 19:02:42 +0900

    Update .config/fish/conf.d/05-basic.fish

--- dot_config/private_fish/conf.d/05-basic.fish
+++ dot_config/private_fish/conf.d/05-basic.fish
@@ -3,9 +3,3 @@ if type -q less
   set -gx LESSHISTFILE -
   set -gx LESS '-gj10 --no-init --quit-if-one-screen --RAW-CONTROL-CHARS'
 end
-
-# editor
-if type -q nvim
-  set -gx EDITOR "nvim"
-  alias vim="nvim"
-end
