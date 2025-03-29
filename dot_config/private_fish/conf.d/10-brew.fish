a0e0e10d01b63acc6939938c535cdefe155804e4
topazape <topazape@gmail.com>
2025-03-29 19:25:01 +0900

    Update .config/fish/conf.d/10-brew.fish

--- dot_config/private_fish/conf.d/10-brew.fish
+++ dot_config/private_fish/conf.d/10-brew.fish
@@ -1,22 +1,22 @@
 # homebrew
 ## linux
 if test -d /home/linuxbrew
-  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 ## mac
 if test -d /opt/homebrew
-  eval (/opt/homebrew/bin/brew shellenv)
+    eval (/opt/homebrew/bin/brew shellenv)
 end
 
 if type -q brew
-  # cache dir
-  if test (uname) = "Darwin"
-    set -gx HOMEBREW_CACHE $XDG_CACHE_HOME/Homebrew
-    set -gx HOMEBREW_LOGS $HOMEBREW_CACHE/Logs
-  end
+    # cache dir
+    if test (uname) = Darwin
+        set -gx HOMEBREW_CACHE $XDG_CACHE_HOME/Homebrew
+        set -gx HOMEBREW_LOGS $HOMEBREW_CACHE/Logs
+    end
 
-  # use bat instead of cat
-  if type -q bat
-    set -gx HOMEBREW_BAT 1
-  end
+    # use bat instead of cat
+    if type -q bat
+        set -gx HOMEBREW_BAT 1
+    end
 end
