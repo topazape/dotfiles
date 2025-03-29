177e30561c18f0e5b177d9c089c87b191d5262a7
topazape <topazape@gmail.com>
2025-03-29 22:26:47 +0900

    Add .config/fish/functions/fish_title.fish

---
+++ dot_config/private_fish/functions/fish_title.fish
@@ -0,0 +1,16 @@
+function fish_title
+    # show ssh hostname if connected
+    set -l ssh ""
+    if set -q SSH_TTY
+        set ssh "["(prompt_hostname | string sub -l 10 | string collect)"] "
+    end
+
+    # show current command if any
+    if set -q argv[1]
+        set -l cmd (string split ' ' $argv[1])[1]
+        echo -- $ssh$cmd
+    else
+        set -l command (status current-command)
+        echo -- $ssh$command
+    end
+end
