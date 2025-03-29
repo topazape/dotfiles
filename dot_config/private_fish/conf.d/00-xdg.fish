f90a31e82632d2918373fe248dd32c763d968213
topazape <topazape@gmail.com>
2025-03-29 19:24:48 +0900

    Update .config/fish/conf.d/00-xdg.fish

--- dot_config/private_fish/conf.d/00-xdg.fish
+++ dot_config/private_fish/conf.d/00-xdg.fish
@@ -1,14 +1,14 @@
 # XDG
 if not test -d $HOME/.config
-  mkdir -p "$HOME/.config"
+    mkdir -p "$HOME/.config"
 end
 
 if not test -d $HOME/.cache
-  mkdir -p "$HOME/.cache"
+    mkdir -p "$HOME/.cache"
 end
 
 if not test -d $HOME/.local/share
-  mkdir -p "$HOME/.local/share"
+    mkdir -p "$HOME/.local/share"
 end
 
 set -gx XDG_CONFIG_HOME $HOME/.config
