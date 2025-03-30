85933378fcb7439c0a025753f4072472ecae48af
topazape <topazape@gmail.com>
2025-03-30 21:53:37 +0900

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+    set -U fish_prompt_pwd_dir_length 0
     starship init fish | source
 end
