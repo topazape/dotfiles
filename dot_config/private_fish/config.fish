0eeddc9601320fc130e6a87b20a0fc4daf8abb0f
topazape <topazape@gmail.com>
2025-03-30 23:03:59 +0900

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,5 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
     starship init fish | source
-    set -U fish_completion_description_max_width 1000
 end
