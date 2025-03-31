07f12633efae2ea45afcacf5cd8a26b16d712fdb
topazape <topazape@gmail.com>
2025-03-31 22:30:16 +0900

    Update .config/wezterm/keymaps.lua

--- dot_config/wezterm/keymaps.lua
+++ dot_config/wezterm/keymaps.lua
@@ -2,7 +2,7 @@ local act = require("wezterm").action
 
 return {
 	-- copy mode
-	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "Space", mods = "LEADER", action = act.ActivateCopyMode },
 	---- copy
 	{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
 	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
