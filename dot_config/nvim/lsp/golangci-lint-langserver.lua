081c4b8d522bdbac4889c9318b11d6eb2e9a92c0
topazape <topazape@gmail.com>
2025-04-08 16:58:16 +0900

    Update .config/nvim/lsp/golangci-lint-langserver.lua

--- dot_config/nvim/lsp/golangci-lint-langserver.lua
+++ dot_config/nvim/lsp/golangci-lint-langserver.lua
@@ -5,7 +5,7 @@ return {
 		command = {
 			"golangci-lint",
 			"run",
-			"--config=/Users/kawai/.config/golangci-lint/config.yml",
+			"--config=/Users/kawai/.config/golangci-lint/golangci.yml",
 			-- "--default=standard",
 			-- "--enable=bodyclose,containedctx,errname,errorlint,exhaustive,gocritic,gosec,makezero,misspell,thelper,wsl",
 			"--output.json.path=stdout",
