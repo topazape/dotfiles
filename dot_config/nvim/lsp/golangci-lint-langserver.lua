389bc93c08bdc6b8bb3fc6b150a4ebe496436c0d
topazape <topazape@gmail.com>
2025-04-08 12:42:41 +0900

    Update .config/nvim/lsp/golangci-lint-langserver.lua

--- dot_config/nvim/lsp/golangci-lint-langserver.lua
+++ dot_config/nvim/lsp/golangci-lint-langserver.lua
@@ -2,28 +2,35 @@ return {
 	cmd = { "golangci-lint-langserver" },
 	cmd_env = { GOLANGCI_LINT_CACHE = vim.env.XDG_CACHE_HOME .. "/golangci-lint" },
 	init_options = {
-		command = (function()
-			if vim.fn.filereadable(vim.env.XDG_CONFIG_HOME .. "/golangci-lint/golangci.yml") == 1 then
-				return {
-					"golangci-lint",
-					"--config",
-					vim.env.XDG_CONFIG_HOME .. "/golangci-lint/golangci.yml",
-					"run",
-					"--output.json.path",
-					"stdout",
-					"--issues-exit-code=1",
-					"--show-stats=false",
-				}
-			end
-			return {
-				"golangci-lint",
-				"run",
-				"--output.json.path",
-				"stdout",
-				"--issues-exit-code=1",
-				"--show-stats=false",
-			}
-		end)(),
+		command = {
+			"golangci-lint",
+			"run",
+			"--output.json.path",
+			"stdout",
+			"--show-stats=false",
+		},
+		-- command = (function()
+		-- 	if vim.fn.filereadable(vim.env.XDG_CONFIG_HOME .. "/golangci-lint/golangci.yml") == 1 then
+		-- 		return {
+		-- 			"golangci-lint",
+		-- 			"--config",
+		-- 			vim.env.XDG_CONFIG_HOME .. "/golangci-lint/golangci.yml",
+		-- 			"run",
+		-- 			"--output.json.path",
+		-- 			"stdout",
+		-- 			"--issues-exit-code=1",
+		-- 			"--show-stats=false",
+		-- 		}
+		-- 	end
+		-- 	return {
+		-- 		"golangci-lint",
+		-- 		"run",
+		-- 		"--output.json.path",
+		-- 		"stdout",
+		-- 		"--issues-exit-code=1",
+		-- 		"--show-stats=false",
+		-- 	}
+		-- end)(),
 	},
 	filetypes = { "go", "gomod" },
 	root_markers = {
