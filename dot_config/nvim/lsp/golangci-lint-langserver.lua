9463415062f3841651a544bbd16324b247cdb681
topazape <topazape@gmail.com>
2025-04-08 12:48:30 +0900

    Update .config/nvim/lsp/golangci-lint-langserver.lua

--- dot_config/nvim/lsp/golangci-lint-langserver.lua
+++ dot_config/nvim/lsp/golangci-lint-langserver.lua
@@ -1,35 +1,9 @@
-local command
-
-if vim.fn.executable("golangci-lint-langserver") == 1 then
-	command = {
-		"golangci-lint",
-		"--config",
-		vim.env.XDG_CONFIG_HOME .. "/golangci-lint/golangci.yml",
-		"run",
-		"--output.json.path",
-		"stdout",
-		"--issues-exit-code=1",
-		"--show-stats=false",
-	}
-else
-	command = {
-		"golangci-lint",
-		"run",
-		"--output.json.path",
-		"stdout",
-		"--issues-exit-code=1",
-		"--show-stats=false",
-	}
-end
-
 return {
 	cmd = { "golangci-lint-langserver" },
 	cmd_env = { GOLANGCI_LINT_CACHE = vim.env.XDG_CACHE_HOME .. "/golangci-lint" },
 	init_options = {
 		command = {
 			"golangci-lint",
-			"--config",
-			vim.env.XDG_CONFIG_HOME .. "/golangci-lint/golangci.yml",
 			"run",
 			"--output.json.path",
 			"stdout",
