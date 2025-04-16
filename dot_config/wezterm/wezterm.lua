49462565f69404f7aeceff23e2888148bc8f7ef0
topazape <topazape@gmail.com>
2025-04-16 21:59:34 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -52,6 +52,7 @@ return {
 
 	-- look and feel
 	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+	integrated_title_buttons = { "Close" },
 	window_background_opacity = 1.00,
 	macos_window_background_blur = 20,
 	use_fancy_tab_bar = true,
