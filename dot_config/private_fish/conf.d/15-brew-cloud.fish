70b6beca803d4f3016fe5e8fb2817150152ac143
topazape <topazape@gmail.com>
2025-04-26 22:00:06 +0900

    Update .config/fish/conf.d/15-brew-cloud.fish

--- dot_config/private_fish/conf.d/15-brew-cloud.fish
+++ dot_config/private_fish/conf.d/15-brew-cloud.fish
@@ -9,8 +9,7 @@ if type -q aws
     set -gx AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
 end
 
-# # google-cloud-sdk
-# if test -d (brew --prefix)/share/google-cloud-sdk
-#     source (brew --prefix)/share/google-cloud-sdk/path.fish.inc
-#     # Google Cloud SDK fish completions should be in (brew --prefix)/share/google-cloud-sdk/completions/
-# end
+# google-cloud-sdk
+if test -d (brew --prefix)/share/google-cloud-sdk
+    source (brew --prefix)/share/google-cloud-sdk/path.fish.inc
+end
