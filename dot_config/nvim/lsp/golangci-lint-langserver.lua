592e4bea14dd95fdca2b80e2cfd73ce8199c6330
topazape <topazape@gmail.com>
2025-04-26 10:29:36 +0900

    Update .config/nvim/lsp/golangci-lint-langserver.lua

--- dot_config/nvim/lsp/golangci-lint-langserver.lua
+++ dot_config/nvim/lsp/golangci-lint-langserver.lua
@@ -1,23 +1,23 @@
 return {
-	cmd = { "golangci-lint-langserver" },
-	cmd_env = { GOLANGCI_LINT_CACHE = vim.env.XDG_CACHE_HOME .. "/golangci-lint" },
-	init_options = {
-		command = {
-			"golangci-lint",
-			"run",
-			"--default=standard",
-			"--enable=bodyclose,containedctx,errname,errorlint,exhaustive,gocritic,gosec,makezero,misspell,thelper,wsl",
-			"--output.json.path=stdout",
-			"--show-stats=false",
-		},
-	},
-	filetypes = { "go", "gomod" },
-	root_markers = {
-		".golangci.yml",
-		".golangci.yaml",
-		".golangci.toml",
-		".golangci.json",
-		"go.work",
-		"go.mod",
-	},
+	-- cmd = { "golangci-lint-langserver" },
+	-- cmd_env = { GOLANGCI_LINT_CACHE = vim.env.XDG_CACHE_HOME .. "/golangci-lint" },
+	-- init_options = {
+	-- 	command = {
+	-- 		"golangci-lint",
+	-- 		"run",
+	-- 		"--default=standard",
+	-- 		"--enable=bodyclose,containedctx,errname,errorlint,exhaustive,gocritic,gosec,makezero,misspell,thelper,wsl",
+	-- 		"--output.json.path=stdout",
+	-- 		"--show-stats=false",
+	-- 	},
+	-- },
+	-- filetypes = { "go", "gomod" },
+	-- root_markers = {
+	-- 	".golangci.yml",
+	-- 	".golangci.yaml",
+	-- 	".golangci.toml",
+	-- 	".golangci.json",
+	-- 	"go.work",
+	-- 	"go.mod",
+	-- },
 }
