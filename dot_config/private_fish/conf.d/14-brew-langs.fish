08ca4bb7843d3835a23b6fe112cff3a591288a5c
topazape <topazape@gmail.com>
2025-04-26 22:01:04 +0900

    Update .config/fish/conf.d/14-brew-langs.fish

--- dot_config/private_fish/conf.d/14-brew-langs.fish
+++ dot_config/private_fish/conf.d/14-brew-langs.fish
@@ -2,7 +2,8 @@
 ## goenv
 if type -q goenv
     set -gx GOENV_ROOT $XDG_DATA_HOME/goenv
-    fish_add_path -p $GOENV_ROOT/shims $GOENV_ROOT/bin
+    # fish_add_path -p $GOENV_ROOT/shims $GOENV_ROOT/bin
+    set -gx $GOENV_ROOT/shims $GOENV_ROOT/bin $PATH
     status --is-interactive; and source (goenv init -|psub)
 end
 ## golang
