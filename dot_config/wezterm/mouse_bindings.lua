4e3badc102f8a14a5e6798af1fff3f4421e682fd
topazape <topazape@gmail.com>
2025-04-29 23:29:51 +0900

    Update .config/wezterm/mouse_bindings.lua

--- dot_config/wezterm/mouse_bindings.lua
+++ dot_config/wezterm/mouse_bindings.lua
@@ -1,1 +1,14 @@
-return {}
+local act = require("wezterm").action
+
+return {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("ClipboardAndPrimarySelection"),
+	},
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "SUPER",
+		action = act.OpenLinkAtMouseCursor,
+	},
+}
