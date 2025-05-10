b54e83a4a61208f8d361ebf6997b2613407c081a
topazape <topazape@gmail.com>
2025-05-10 22:56:25 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -290,8 +290,8 @@ return {
 			-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
 			-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
 			-- refer to `:h file-pattern` for more examples
-			"BufReadPre path/to/my-vault/*.md",
-			"BufNewFile path/to/my-vault/*.md",
+			"BufReadPre ~/vault/*.md",
+			"BufNewFile ~/vault/*.md",
 		},
 		dependencies = {
 			-- Required.
