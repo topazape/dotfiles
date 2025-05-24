93334bcb90be30108acffeaaf71467960e8c96b4
topazape <topazape@gmail.com>
2025-05-24 23:06:18 +0900

    Update .config/fish/functions/fish_title.fish

--- dot_config/private_fish/functions/fish_title.fish
+++ dot_config/private_fish/functions/fish_title.fish
@@ -1,6 +1,7 @@
 function fish_title
     # show ssh hostname if connected
     set -l ssh ""
+    # check if we are in an SSH session
     if set -q SSH_TTY
         set ssh "["(prompt_hostname | string sub -l 10 | string collect)"] "
     end
@@ -10,8 +11,8 @@ function fish_title
         set -l cmd (string split ' ' $argv[1])[1]
         echo -- $ssh$cmd
     else
-        set -l command (status current-command)
+        # set -l command (status current-command)
         # echo -- $ssh$command
-        echo -- $ssh"> "
+        echo -- $ssh
     end
 end
