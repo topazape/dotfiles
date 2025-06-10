3f06a7f9a87b01f27612532acf2d876fa421cd4a
topazape <topazape@gmail.com>
2025-06-10 14:54:15 +0900

    Update .config/nvim/lsp/yaml-language-server.lua

--- dot_config/nvim/lsp/yaml-language-server.lua
+++ dot_config/nvim/lsp/yaml-language-server.lua
@@ -1,10 +1,10 @@
 return {
-	-- cmd = { "yaml-language-server", "--stdio" },
-	-- filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
-	-- settings = {
-	-- 	schemaStore = {
-	-- 		enable = true,
-	-- 		url = "https://www.schemastore.org/api/json/catalog.json",
-	-- 	},
-	-- },
+	cmd = { "yaml-language-server", "--stdio" },
+	filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
+	settings = {
+		schemaStore = {
+			enable = true,
+			url = "https://www.schemastore.org/api/json/catalog.json",
+		},
+	},
 }
