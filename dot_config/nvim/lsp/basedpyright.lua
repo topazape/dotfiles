4e1e36b6d9c0f0b90a009e70ada3c65a024638d2
topazape <topazape@gmail.com>
2025-07-04 23:06:43 +0900

    Update .config/nvim/lsp/basedpyright.lua

--- dot_config/nvim/lsp/basedpyright.lua
+++ dot_config/nvim/lsp/basedpyright.lua
@@ -13,7 +13,7 @@ return {
 	settings = {
 		basedpyright = {
 			analysis = {
-				typeCheckingMode = "off", -- off, basic, standard, strict, recommended, all
+				typeCheckingMode = "basic", -- off, basic, standard, strict, recommended, all
 				diagnosticMode = "workspace",
 				inlayHints = {
 					callArgumentNames = false,
