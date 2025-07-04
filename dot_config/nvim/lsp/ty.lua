1f00244595010b505a45adcf0a9427d04efb6f27
topazape <topazape@gmail.com>
2025-07-04 22:57:59 +0900

    Update .config/nvim/lsp/ty.lua

--- dot_config/nvim/lsp/ty.lua
+++ dot_config/nvim/lsp/ty.lua
@@ -1,14 +1,14 @@
 return {
-	-- cmd = { "ty", "server" },
-	-- filetypes = { "python" },
-	-- root_markers = {
-	-- 	"Pipfile",
-	-- 	"pyproject.toml",
-	-- 	"requirements.txt",
-	-- 	"setup.cfg",
-	-- 	"setup.py",
-	-- 	"ty.toml",
-	-- 	".git",
-	-- },
-	-- settings = {},
+	cmd = { "ty", "server" },
+	filetypes = { "python" },
+	root_markers = {
+		"Pipfile",
+		"pyproject.toml",
+		"requirements.txt",
+		"setup.cfg",
+		"setup.py",
+		"ty.toml",
+		".git",
+	},
+	settings = {},
 }
