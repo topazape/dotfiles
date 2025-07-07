987d689946c3ee19664324e823f74299e9d3d28c
topazape <topazape@gmail.com>
2025-07-07 22:09:56 +0900

    Update .config/nvim/lsp/basedpyright.lua

--- dot_config/nvim/lsp/basedpyright.lua
+++ dot_config/nvim/lsp/basedpyright.lua
@@ -1,27 +1,37 @@
 return {
-	-- cmd = { "basedpyright-langserver", "--stdio" },
-	-- filetypes = { "python" },
-	-- root_markers = {
-	-- 	"Pipfile",
-	-- 	"pyproject.toml",
-	-- 	"pyrightconfig.json",
-	-- 	"requirements.txt",
-	-- 	"setup.cfg",
-	-- 	"setup.py",
-	-- 	".git",
-	-- },
-	-- settings = {
-	-- 	basedpyright = {
-	-- 		analysis = {
-	-- 			typeCheckingMode = "strict", -- off, basic, standard, strict, recommended, all
-	-- 			diagnosticMode = "workspace",
-	-- 			inlayHints = {
-	-- 				callArgumentNames = false,
-	-- 				functionReturnTypes = false,
-	-- 				genericTypes = true,
-	-- 				variableTypes = true,
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
+	cmd = { "basedpyright-langserver", "--stdio" },
+	filetypes = { "python" },
+	root_markers = {
+		"Pipfile",
+		"pyproject.toml",
+		"pyrightconfig.json",
+		"requirements.txt",
+		"setup.cfg",
+		"setup.py",
+	},
+	settings = {
+		basedpyright = {
+			analysis = {
+				typeCheckingMode = "recommended", -- off, basic, standard, strict, recommended, all
+				diagnosticMode = "workspace",
+				diagnosticSeverityOverrides = {
+					reportAny = false,
+					reportAttributeAccessIssue = false,
+					reportCallInDefaultInitializer = false,
+					reportExplicitAny = false,
+					reportUnknownArgumentType = false,
+					reportUnknownMemberType = false,
+					reportUnknownVariableType = false,
+					reportUnusedImport = false,
+					reportUnusedVariable = false,
+				},
+				inlayHints = {
+					callArgumentNames = false,
+					functionReturnTypes = false,
+					genericTypes = true,
+					variableTypes = true,
+				},
+			},
+		},
+	},
 }
