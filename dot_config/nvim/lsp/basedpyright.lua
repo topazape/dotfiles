6456d45e6e376ef01cfd287e8e2ed85bd3dea348
topazape <topazape@gmail.com>
2025-07-07 22:05:57 +0900

    Update .config/nvim/lsp/basedpyright.lua

--- dot_config/nvim/lsp/basedpyright.lua
+++ dot_config/nvim/lsp/basedpyright.lua
@@ -1,27 +1,27 @@
 return {
-	cmd = { "basedpyright-langserver", "--stdio" },
-	filetypes = { "python" },
-	root_markers = {
-		"Pipfile",
-		"pyproject.toml",
-		"pyrightconfig.json",
-		"requirements.txt",
-		"setup.cfg",
-		"setup.py",
-		".git",
-	},
-	settings = {
-		basedpyright = {
-			analysis = {
-				typeCheckingMode = "strict", -- off, basic, standard, strict, recommended, all
-				diagnosticMode = "workspace",
-				inlayHints = {
-					callArgumentNames = false,
-					functionReturnTypes = false,
-					genericTypes = true,
-					variableTypes = true,
-				},
-			},
-		},
-	},
+	-- cmd = { "basedpyright-langserver", "--stdio" },
+	-- filetypes = { "python" },
+	-- root_markers = {
+	-- 	"Pipfile",
+	-- 	"pyproject.toml",
+	-- 	"pyrightconfig.json",
+	-- 	"requirements.txt",
+	-- 	"setup.cfg",
+	-- 	"setup.py",
+	-- 	".git",
+	-- },
+	-- settings = {
+	-- 	basedpyright = {
+	-- 		analysis = {
+	-- 			typeCheckingMode = "strict", -- off, basic, standard, strict, recommended, all
+	-- 			diagnosticMode = "workspace",
+	-- 			inlayHints = {
+	-- 				callArgumentNames = false,
+	-- 				functionReturnTypes = false,
+	-- 				genericTypes = true,
+	-- 				variableTypes = true,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
