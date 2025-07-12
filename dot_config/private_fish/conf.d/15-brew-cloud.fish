7e1b9dabd3284c0b5c238d20ca4b795c73ffcefb
topazape <topazape@gmail.com>
2025-07-12 19:33:10 +0900

    Update .config/fish/conf.d/15-brew-cloud.fish

--- dot_config/private_fish/conf.d/15-brew-cloud.fish
+++ dot_config/private_fish/conf.d/15-brew-cloud.fish
@@ -8,8 +8,3 @@ if type -q aws
     set -gx AWS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
     set -gx AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
 end
-
-# google-cloud-sdk
-if test -d (brew --prefix)/share/google-cloud-sdk
-    source (brew --prefix)/share/google-cloud-sdk/path.fish.inc
-end
