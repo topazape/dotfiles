3f0c56f25b1fd393d68e10cb6426215334f31cc5
topazape <topazape@gmail.com>
2025-08-25 14:49:23 +0900

    Update .config/fish/conf.d/90-apps.fish

--- dot_config/private_fish/conf.d/90-apps.fish
+++ dot_config/private_fish/conf.d/90-apps.fish
@@ -3,18 +3,3 @@ if type -q chezmoi
     # completion
     chezmoi completion fish | source
 end
-
-# gtrash (https://github.com/umlx5h/gtrash)
-if type -q gtrash
-    gtrash completion fish | source
-    alias tm="gtrash put"
-    function rm
-        echo 'If you want to use rm really, then use "command rm" instead.'
-        return 1
-    end
-end
-
-# claude code
-if type -q claude
-    set -gx CLAUDE_CONFIG_DIR $XDG_CONFIG_HOME/claude
-end
