38fdf63bdf4b2de8a788c92fe89fccc8ad305f35
topazape <topazape@gmail.com>
2025-09-10 12:22:15 +0900

    Update .config/wezterm/keymaps.lua

--- dot_config/wezterm/keymaps.lua
+++ dot_config/wezterm/keymaps.lua
@@ -2,7 +2,7 @@ local act = require("wezterm").action
 
 return {
 	-- generic
-	{ key = "Enter", mods = "SHIFT", action = require("wezterm").action.SendString("\n") },
+	{ key = "Enter", mods = "SHIFT", action = act.SendString("\x1b\r") },
 	-- copy mode
 	{ key = "Space", mods = "LEADER", action = act.ActivateCopyMode },
 	---- copy & paste
