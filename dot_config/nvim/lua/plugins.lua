2dbb92569de412bd3c7fd87d233e8f231c1b06ff
topazape <topazape@gmail.com>
2025-09-18 22:06:17 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -61,16 +61,20 @@ return {
 	{
 		"folke/trouble.nvim",
 		opts = {
-			mode = "diagnostics",
-			preview = {
-				type = "float",
-				relative = "editor",
-				border = "rounded",
-				title = "Preview",
-				title_pos = "center",
-				position = { 0, -2 },
-				size = { width = 0.3, height = 0.3 },
-				zindex = 200,
+			modes = {
+				preview_float = {
+					mode = "diagnostics",
+					preview = {
+						type = "float",
+						relative = "editor",
+						border = "rounded",
+						title = "Preview",
+						title_pos = "center",
+						position = { 0, -2 },
+						size = { width = 0.3, height = 0.3 },
+						zindex = 200,
+					},
+				},
 			},
 		},
 		cmd = "Trouble",
