e1bf3a0cbdfbbfeb223b92ff35f80f5e65c72ac2
topazape <topazape@gmail.com>
2025-10-08 16:58:54 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -196,7 +196,7 @@ return {
 		opts = {
 			terminal = {
 				split_side = "right", -- "left" or "right"
-				split_width_percentage = 0.30,
+				split_width_percentage = 0.40,
 				provider = "auto", -- "auto", "snacks", "native", "external", "none", or custom provider table
 				auto_close = true,
 				snacks_win_opts = {}, -- Opts to pass to `Snacks.terminal.open()` - see Floating Window section below
