4d5ffb8d42d6cf3d07f64b2b1efcfc67d5cd7b95
topazape <topazape@gmail.com>
2025-10-16 16:21:31 +0900

    Add .config/nvim/lsp/ty.lua

---
+++ dot_config/nvim/lsp/ty.lua
@@ -0,0 +1,13 @@
+return {
+	cmd = { "ty", "server" },
+	filetypes = { "python" },
+	root_markers = {
+		"Pipfile",
+		"pyproject.toml",
+		"pyrightconfig.json",
+		"requirements.txt",
+		"setup.cfg",
+		"setup.py",
+		"ty.toml",
+	},
+}
