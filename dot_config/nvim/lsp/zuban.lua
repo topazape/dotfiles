3a63b7ce98264a988bb8a44bc9b97ca9dd11b2bb
topazape <topazape@gmail.com>
2025-10-16 16:19:38 +0900

    Add .config/nvim/lsp/zuban.lua

---
+++ dot_config/nvim/lsp/zuban.lua
@@ -0,0 +1,12 @@
+return {
+	cmd = { "zuban", "server" },
+	filetypes = { "python" },
+	root_markers = {
+		"Pipfile",
+		"pyproject.toml",
+		"pyrightconfig.json",
+		"requirements.txt",
+		"setup.cfg",
+		"setup.py",
+	},
+}
