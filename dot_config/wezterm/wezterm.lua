c43939feafbe295ba51aac07b4917ed805644731
topazape <topazape@gmail.com>
2025-11-05 20:40:26 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local FONT_SIZE = 16
 local CONSOLE_FONTS = {
 	{ family = "Berkeley Mono" },
 	{ family = "Maple Mono" },
+	{ family = "IBM Plex Sans JP" },
 	{ family = "WDXL Lubrifont JP N" },
 	{ family = "Pretendard JP" },
 }
