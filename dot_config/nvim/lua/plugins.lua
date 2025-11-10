5c77c049529896d7d23a180c2bf599f33607ad58
topazape <topazape@gmail.com>
2025-11-10 20:51:39 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -109,12 +109,7 @@ return {
 			explorer = { enabled = true },
 			indent = { enabled = true },
 			input = { enabled = true },
-			picker = {
-				enabled = true,
-				win = { input = { keys = {
-					["<Esc>"] = { "close", mode = "n", "i" },
-				} } },
-			},
+			picker = { enabled = true },
 			notifier = { enabled = true },
 			quickfile = { enabled = true },
 			scope = { enabled = true },
