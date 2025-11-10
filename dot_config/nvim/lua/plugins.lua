bb96277dddeb870e8e5cb74a709445ef3831ea4b
topazape <topazape@gmail.com>
2025-11-10 09:55:55 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -103,9 +103,20 @@ return {
 		"folke/snacks.nvim",
 		priority = 1000,
 		lazy = false,
-		config = function()
-			require("config/snacks")
-		end,
+		opt = {
+			bigfile = { enabled = true },
+			dashboard = { enabled = false },
+			explorer = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
 	},
 
 	-- UI
