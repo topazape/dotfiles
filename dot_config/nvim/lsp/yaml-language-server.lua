2903dd981897f12dd78c5a39393653e133b702e1
topazape <topazape@gmail.com>
2025-11-11 20:52:11 +0900

    Update .config/nvim/lsp/yaml-language-server.lua

--- dot_config/nvim/lsp/yaml-language-server.lua
+++ dot_config/nvim/lsp/yaml-language-server.lua
@@ -8,16 +8,16 @@ return {
 				url = "",
 			},
 			schemas = require("schemastore").yaml.schemas({
-				extra = {
+				replace = {
 					["Argo Events"] = {
 						description = "Argo Events Event Sources and Sensors",
-						fileMatch = { "**/*manifest*/**/*.yml" },
+						fileMatch = { "**/*manifest*/**/cv.yml" },
 						name = "Argo Events",
 						url = "https://raw.githubusercontent.com/argoproj/argo-events/master/api/jsonschema/schema.json",
 					},
 					["Argo Workflows"] = {
 						description = "Argo Workflow configuration file",
-						fileMatch = { "**/*manifest*/**/*.yml" },
+						fileMatch = { "**/*manifest*/**/cv.yml" },
 						name = "Argo Workflows",
 						url = "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json",
 					},
