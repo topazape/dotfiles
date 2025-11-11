e1775432b54cd798503f290d03660945a43f64c7
topazape <topazape@gmail.com>
2025-11-11 20:30:05 +0900

    Update .config/nvim/lsp/yaml-language-server.lua

--- dot_config/nvim/lsp/yaml-language-server.lua
+++ dot_config/nvim/lsp/yaml-language-server.lua
@@ -2,17 +2,19 @@ return {
 	cmd = { "yaml-language-server", "--stdio" },
 	filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
 	settings = {
-		schemaStore = {
-			enable = false,
-			url = "",
+		yaml = {
+			schemaStore = {
+				enable = false,
+				url = "",
+			},
+			schemas = require("schemastore").yaml.schemas({
+				["Argo Events"] = {
+					fileMatch = { "**/*manifest*/*.yml" },
+				},
+				["Argo Workflows"] = {
+					fileMatch = { "**/*manifest*/*.yml" },
+				},
+			}),
 		},
-		schemas = require("schemastore").yaml.schemas({
-			["Argo Events"] = {
-				fileMatch = { "**/*manifest*/*.yml" },
-			},
-			["Argo Workflows"] = {
-				fileMatch = { "**/*manifest*/*.yml" },
-			},
-		}),
 	},
 }
