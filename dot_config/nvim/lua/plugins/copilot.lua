a75b2ce42ffd72d2c9df733456f2ccac91c973de
topazape <topazape@gmail.com>
2025-11-12 22:26:13 +0900

    Update .config/nvim/lua/plugins/copilot.lua

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,31 +1,31 @@
 return {
-			"zbirenbaum/copilot.lua",
-			opts = {
-				filetypes = {
-					txt = true,
-					markdown = true,
-					json = true,
-					yaml = true,
-					toml = true,
-					sql = true,
-					dbt = true,
-					["*"] = false,
-				},
-				panel = {
-					enabled = false,
-				},
-				suggestion = {
-					-- enabled = true,
-					enabled = false,
-					auto_trigger = true,
-					hide_during_completion = true,
-					debounce = 25,
-					keymap = {
-						accept = "<M-f>",
-						next = "<M-n>",
-						prev = "<M-p>",
-						dissmiss = "<M-q>",
-					},
-				},
+	"zbirenbaum/copilot.lua",
+	opts = {
+		filetypes = {
+			txt = true,
+			markdown = true,
+			json = true,
+			yaml = true,
+			toml = true,
+			sql = true,
+			dbt = true,
+			["*"] = false,
+		},
+		panel = {
+			enabled = false,
+		},
+		suggestion = {
+			-- enabled = true,
+			enabled = false,
+			auto_trigger = true,
+			hide_during_completion = true,
+			debounce = 25,
+			keymap = {
+				accept = "<M-f>",
+				next = "<M-n>",
+				prev = "<M-p>",
+				dissmiss = "<M-q>",
 			},
-		}
+		},
+	},
+}
