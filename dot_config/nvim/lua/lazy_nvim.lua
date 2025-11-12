629d6ebea2999680000f967ec01c420a535195df
topazape <topazape@gmail.com>
2025-11-12 22:56:26 +0900

    Update .config/nvim/lua/lazy_nvim.lua

--- dot_config/nvim/lua/lazy_nvim.lua
+++ dot_config/nvim/lua/lazy_nvim.lua
@@ -18,100 +18,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		{
-			"folke/ts-comments.nvim",
-			event = "VeryLazy",
-			config = true,
-			enable = vim.fn.has("nvim-0.10.0") == 1,
-		},
-		{ "nvim-tree/nvim-web-devicons", config = true },
-		{ "HiPhish/rainbow-delimiters.nvim" },
-		{ "williamboman/mason.nvim", config = true },
-		{ "j-hui/fidget.nvim", config = true },
-		{ "b0o/schemastore.nvim" },
-		{ "OXY2DEV/helpview.nvim" },
-		{ "tzachar/highlight-undo.nvim", config = true },
-		{
-			"brenoprata10/nvim-highlight-colors",
-			opts = {
-				render = "virtual",
-				virtual_symbol = "",
-				virtual_symbol_prefix = " ",
-				virtual_symbol_position = "eol",
-				exclude_filetypes = { "lazy", "mason" },
-			},
-		},
-
-		-- git
-		---- conflict
-		{
-			"akinsho/git-conflict.nvim",
-			version = "*",
-			config = true,
-		},
-
-		-- languages
-		---- golang
-		{
-			"fredrikaverpil/godoc.nvim",
-			version = "*",
-			dependencies = {
-				"folke/snacks.nvim",
-			},
-			opts = {
-				picker = { type = "snacks" },
-			},
-		},
-		---- typst
-		{
-			"chomosuke/typst-preview.nvim",
-			ft = "typst", -- or ft = 'typst'
-			version = "1.*",
-			build = function()
-				require("typst-preview").update()
-			end,
-		},
-
-		---- dbt
-		{
-			"PedramNavid/dbtpal",
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-			},
-			config = true,
-		},
-
-		{
-			"MeanderingProgrammer/render-markdown.nvim",
-			dependencies = {
-				"nvim-treesitter/nvim-treesitter",
-				"nvim-tree/nvim-web-devicons",
-			},
-			ft = { "markdown", "Avante" },
-			opts = {
-				heading = {
-					position = "inline",
-				},
-				code = {
-					width = "block",
-					left_margin = 2,
-				},
-				bullet = {
-					icons = { "•", "∘", "▪", "▫" },
-				},
-				completions = {
-					lsp = { enabled = true },
-				},
-			},
-		},
-		{
-			"rainbowhxch/accelerated-jk.nvim",
-			config = function()
-				vim.api.nvim_set_keymap("n", "j", "<Plug>(accelerated_jk_gj)", {})
-				vim.api.nvim_set_keymap("n", "k", "<Plug>(accelerated_jk_gk)", {})
-			end,
-		},
-
 		{ import = "plugins.oldtale" },
 		{ import = "plugins.treesitter" },
 		{ import = "plugins.blink-cmp" },
@@ -122,5 +28,35 @@ require("lazy").setup({
 		{ import = "plugins.lualine" },
 		{ import = "plugins.copilot" },
 		{ import = "plugins.claudecode" },
+		{ import = "plugins.render-markdown" },
+
+		{ "folke/ts-comments.nvim", event = "VeryLazy", config = true },
+		{ "nvim-tree/nvim-web-devicons", config = true },
+		{ "HiPhish/rainbow-delimiters.nvim" },
+		{ "williamboman/mason.nvim", config = true },
+		{ "j-hui/fidget.nvim", config = true },
+		{ "b0o/schemastore.nvim" },
+		{ "OXY2DEV/helpview.nvim" },
+		{ "tzachar/highlight-undo.nvim", config = true },
+		{ "akinsho/git-conflict.nvim", version = "*", config = true },
+		{ "chomosuke/typst-preview.nvim", ft = "typst", version = "1.*", config = true },
+		{ "PedramNavid/dbtpal", dependencies = { "nvim-lua/plenary.nvim" }, config = true },
+		{
+			"fredrikaverpil/godoc.nvim",
+			version = "*",
+			dependencies = {
+				"folke/snacks.nvim",
+			},
+			opts = {
+				picker = { type = "snacks" },
+			},
+		},
+		{
+			"rainbowhxch/accelerated-jk.nvim",
+			config = function()
+				vim.api.nvim_set_keymap("n", "j", "<Plug>(accelerated_jk_gj)", {})
+				vim.api.nvim_set_keymap("n", "k", "<Plug>(accelerated_jk_gk)", {})
+			end,
+		},
 	},
 })
