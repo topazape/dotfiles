6038c85c8fc25535ef0ed3c1a7f064acf1d68ecf
topazape <topazape@gmail.com>
2025-11-12 22:19:22 +0900

    Update .config/nvim/lua/plugins/conform.lua

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,10 +1,11 @@
 return {
 	"stevearc/conform.nvim",
+
 	opts = {
 		format_on_save = {
 			timeout_ms = 3000,
 			lsp_format = "fallback",
-		},
+    },
 		formatters = {
 			["sqlfluff"] = {
 				require_cwd = false,
