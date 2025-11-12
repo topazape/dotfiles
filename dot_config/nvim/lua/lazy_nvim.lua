5a5babe9151c221405fafeea714fb2d417e37a6c
topazape <topazape@gmail.com>
2025-11-12 22:44:34 +0900

    Update .config/nvim/lua/lazy_nvim.lua Update .config/nvim/lua/plugins/oldtale.lua

--- dot_config/nvim/lua/lazy_nvim.lua
+++ dot_config/nvim/lua/lazy_nvim.lua
@@ -19,31 +19,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
 		{
-			dir = "~/Develop/local/oldtale.nvim",
-			lazy = false,
-			priority = 1000,
-			config = function()
-				require("oldtale").setup({
-					integrations = {
-						blink = true,
-						gitsigns = true,
-						lazy = true,
-						lsp = true,
-						markdown = true,
-						mason = true,
-						noice = true,
-						notify = true,
-						octo = true,
-						rainbow_delimiters = true,
-						saga = true,
-						telescope = false,
-						treesitter = true,
-					},
-				})
-				vim.cmd.colorscheme("oldtale")
-			end,
-		},
-		{
 			"folke/ts-comments.nvim",
 			event = "VeryLazy",
 			config = true,
--- dot_config/nvim/lua/plugins/oldtale.lua
+++ dot_config/nvim/lua/plugins/oldtale.lua
@@ -1,1 +1,25 @@
-return {}
+return {
+	dir = "~/Develop/local/oldtale.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("oldtale").setup({
+			integrations = {
+				blink = true,
+				gitsigns = true,
+				lazy = true,
+				lsp = true,
+				markdown = true,
+				mason = true,
+				noice = true,
+				notify = true,
+				octo = true,
+				rainbow_delimiters = true,
+				saga = true,
+				telescope = false,
+				treesitter = true,
+			},
+		})
+		vim.cmd.colorscheme("oldtale")
+	end,
+}
