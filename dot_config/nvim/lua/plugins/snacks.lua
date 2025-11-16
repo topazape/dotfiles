6c36da039d4d5243576b4489a75320386f0d3ce5
topazape <topazape@gmail.com>
2025-11-16 21:42:23 +0900

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
 		indent = {
 			enabled = true,
 			indent = { char = "┊", hl = "LineNr" },
-			scope = { char = "│", hl = "LineNr" },
+			scope = { char = "│", hl = "CursorLineNr" },
 		},
 		input = { enabled = true },
 		notifier = { enabled = true },
