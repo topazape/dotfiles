e838eb411c98ed44dae8d8817e45dda614f89b8d
topazape <topazape@gmail.com>
2025-11-16 21:39:50 +0900

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
 		indent = {
 			enabled = true,
 			indent = { char = "│" },
-			scope = { char = "┊", hl = "Search" },
+			scope = { char = "┊", hl = "Visual" },
 		},
 		input = { enabled = true },
 		notifier = { enabled = true },
