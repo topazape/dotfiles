4cae3e217e4f475b26ea57c60734153025d155e9
topazape <topazape@gmail.com>
2025-11-18 21:33:59 +0900

    Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,4 +3,12 @@ return {
 	lazy = false,
 	branch = "main",
 	build = ":TSUpdate",
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+	end,
 }
