575a0a8d3ef9c071659d4b9d77414f06c963faf0
topazape <topazape@gmail.com>
2025-11-18 19:52:12 +0900

    Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,4 +3,10 @@ return {
 	lazy = false,
 	branch = "main",
 	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").setup({
+			-- Directory to install parsers and queries to
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
+	end,
 }
