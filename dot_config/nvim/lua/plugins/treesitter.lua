79dcfeeae695dcbcc3a2aaa0abb7387c054a6e03
topazape <topazape@gmail.com>
2025-11-18 19:39:55 +0900

    Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,22 +4,6 @@ return {
 	branch = "main",
 	build = ":TSUpdate",
 	opts = {
-		sync_install = false,
-		auto_install = false,
-
-		highlight = {
-			enable = true,
-			disable = {},
-			additional_vim_regex_highlighting = true,
-		},
-
-		indent = {
-			enable = true,
-			disable = {
-				"yaml",
-			},
-		},
-
 		ensure_installed = "all",
 	},
 }
