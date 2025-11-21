dcb3cfcb6f08aa880a079ec8d6c89c14237c7b3e
topazape <topazape@gmail.com>
2025-11-21 22:33:08 +0900

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -17,7 +17,7 @@ return {
 			enabled = true,
 			margin = { top = 0, left = 1, right = 0, bottom = 0 },
 			gap = 1,
-			top_down = false,
+			-- top_down = false,
 			style = "fancy",
 			zindex = 10,
 		},
