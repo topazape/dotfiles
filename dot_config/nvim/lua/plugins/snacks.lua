8abc13be5c50f2f10147670239fceaa7f1aa37f1
topazape <topazape@gmail.com>
2025-11-21 22:34:48 +0900

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -15,11 +15,7 @@ return {
 		input = { enabled = true },
 		notifier = {
 			enabled = true,
-			margin = { top = 0, left = 1, right = 0, bottom = 0 },
-			gap = 1,
-			-- top_down = false,
-			-- style = "fancy",
-			zindex = 40,
+			style = "fancy",
 		},
 		picker = {
 			win = {
