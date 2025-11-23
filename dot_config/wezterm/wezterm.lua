ff6340cf56b133e68a8da71a5863a946c54f30ce
topazape <topazape@gmail.com>
2025-11-23 14:27:55 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local FONT_SIZE = 16
 local CONSOLE_FONTS = {
 	{ family = "Berkeley Mono" },
 	{ family = "Maple Mono" },
-	{ family = "LINE Seed JP" },
+	{ family = "LINE Seed JP", weight = "Thin" },
 }
 
 local function retrieve_font_settings(fonts, weight)
