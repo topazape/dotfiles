63d5f8332ca41a29f0b6c01b51414d80385a3df9
topazape <topazape@gmail.com>
2025-12-08 21:35:04 +0900

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -104,15 +104,65 @@ return {
 			end,
 			desc = "Delete Buffer",
 		},
+		-- git
 		{
-			"<leader>tb",
+			"<leader>gb",
+			function()
+				Snacks.picker.git_branches()
+			end,
+			desc = "Git Branches",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.picker.git_log()
+			end,
+			desc = "Git Log",
+		},
+		{
+			"<leader>gL",
+			function()
+				Snacks.picker.git_log_line()
+			end,
+			desc = "Git Log Line",
+		},
+		{
+			"<leader>gs",
+			function()
+				Snacks.picker.git_status()
+			end,
+			desc = "Git Status",
+		},
+		{
+			"<leader>gS",
+			function()
+				Snacks.picker.git_stash()
+			end,
+			desc = "Git Stash",
+		},
+		{
+			"<leader>gd",
+			function()
+				Snacks.picker.git_diff()
+			end,
+			desc = "Git Diff (Hunks)",
+		},
+		{
+			"<leader>gf",
+			function()
+				Snacks.picker.git_log_file()
+			end,
+			desc = "Git Log File",
+		},
+		{
+			"<leader>gt",
 			function()
 				Snacks.git.blame_line()
 			end,
 			desc = "Git Blame",
 		},
 		{
-			"<leader>gg",
+			"<leader>gl",
 			function()
 				Snacks.lazygit()
 			end,
