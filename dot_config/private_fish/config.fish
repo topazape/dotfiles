056cb5e03fd4de4856235840538f6352a577eef7
topazape <topazape@gmail.com>
2025-12-18 22:14:16 +0900

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,3 @@ if status is-interactive
     set fish_greeting
     starship init fish | source
 end
-
-# Limit fish-lsp indexing to user config only (skip $__fish_data_dir for faster startup)
-set -gx fish_lsp_all_indexed_paths "$__fish_config_dir"
