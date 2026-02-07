d6f2721654c102b35b621c08ea11dda63ca88d8e
topazape <topazape@gmail.com>
2026-02-07 23:48:50 +0900

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -41,7 +41,7 @@ return {
 						if item.file then
 							local bufnr = vim.fn.bufnr(item.file)
 							if bufnr ~= -1 and vim.bo[bufnr].modified then
-								table.insert(ret, { " ", "DiagnosticWarn" })
+								table.insert(ret, { "󰛄 ", "DiagnosticWarn" })
 							end
 						end
 						return ret
