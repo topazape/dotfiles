fc3042940d61cb0dee1dfdefe39e5bab5a958395
topazape <topazape@gmail.com>
2026-02-09 11:21:15 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -47,8 +47,8 @@ return {
 	font = wezterm.font_with_fallback(retrieve_font_settings(CONSOLE_FONTS, "Regular")),
 	font_size = FONT_SIZE,
 	warn_about_missing_glyphs = false,
-	cell_width = 1.05,
-	line_height = 1.05,
+	cell_width = 1.10,
+	line_height = 1.10,
 	adjust_window_size_when_changing_font_size = false,
 
 	-- look and feel
