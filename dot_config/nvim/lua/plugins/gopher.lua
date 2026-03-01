8732df1a54e621dd61d9a16fff2c973a772c8b1e
topazape <topazape@gmail.com>
2026-03-02 08:43:17 +0900

    Update .config/nvim/lua/plugins/gopher.lua

--- dot_config/nvim/lua/plugins/gopher.lua
+++ dot_config/nvim/lua/plugins/gopher.lua
@@ -2,7 +2,8 @@ return {
 	"olexsmir/gopher.nvim",
 	ft = "go",
 	build = function()
-		require("gopher.installer").install_deps()
+		-- require("gopher.installer").install_deps()
+		vim.cmd.GoInstallDeps()
 	end,
 	opts = {},
 }
