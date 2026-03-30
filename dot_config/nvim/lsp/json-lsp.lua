89bf86cceab8708227d0d238ee5b7c854bd7035f
topazape <topazape@gmail.com>
2026-03-30 17:34:24 +0900

    Update .config/nvim/lsp/json-lsp.lua

--- dot_config/nvim/lsp/json-lsp.lua
+++ dot_config/nvim/lsp/json-lsp.lua
@@ -2,9 +2,9 @@ return {
 	cmd = { "vscode-json-language-server", "--stdio" },
 	filetypes = { "json", "jsonc" },
 	settings = {
-		json = {
-			schemas = require("schemastore").json.schemas(),
-			validate = { enable = true },
-		},
+		-- json = {
+		-- 	schemas = require("schemastore").json.schemas(),
+		-- 	validate = { enable = true },
+		-- },
 	},
 }
