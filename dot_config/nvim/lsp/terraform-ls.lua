6eb14ff59b819fd34281d6162e861e05e3e17368
topazape <topazape@gmail.com>
2026-03-31 10:01:12 +0900

    Update .config/nvim/lsp/terraform-ls.lua

--- dot_config/nvim/lsp/terraform-ls.lua
+++ dot_config/nvim/lsp/terraform-ls.lua
@@ -1,8 +1,8 @@
 return {
 	cmd = { "terraform-ls", "serve" },
 	filetypes = { "terraform", "terraform-vars", "hcl" },
-	init_options = {
-		ignoreSingleFileWarning = true,
-	},
+	-- init_options = {
+	-- 	ignoreSingleFileWarning = true,
+	-- },
 	root_markers = { ".terraform", ".git" },
 }
