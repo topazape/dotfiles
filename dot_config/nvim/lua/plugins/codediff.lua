acc11691c2f0112dcdc2d5665390a802b16850a6
topazape <topazape@gmail.com>
2026-04-15 08:59:59 +0900

    Update .config/nvim/lua/plugins/codediff.lua

--- dot_config/nvim/lua/plugins/codediff.lua
+++ dot_config/nvim/lua/plugins/codediff.lua
@@ -1,4 +1,12 @@
 return {
 	"esmuellert/codediff.nvim",
 	cmd = "CodeDiff",
+	opts = {
+		diff = {},
+		explorer = {
+			width = 30,
+			view_mode = "tree",
+			focus_on_select = true,
+		},
+	},
 }
