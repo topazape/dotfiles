3b4341cc3137ec3de2c151817f904805593fcdab
topazape <topazape@gmail.com>
2026-04-15 10:39:28 +0900

    Update .config/nvim/lua/plugins/codediff.lua

--- dot_config/nvim/lua/plugins/codediff.lua
+++ dot_config/nvim/lua/plugins/codediff.lua
@@ -2,11 +2,19 @@ return {
 	"esmuellert/codediff.nvim",
 	cmd = "CodeDiff",
 	opts = {
-		diff = {},
+		diff = {
+			compute_moves = true,
+		},
 		explorer = {
 			width = 30,
 			view_mode = "tree",
+			flatten_dirs = false,
 			focus_on_select = true,
+		},
+		keymaps = {
+			explorer = {
+				select = "<TAB>",
+			},
 		},
 	},
 }
