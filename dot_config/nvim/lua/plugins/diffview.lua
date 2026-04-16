4174d037d1cdf46a243c02a115839e1b877bafda
topazape <topazape@gmail.com>
2026-04-16 16:24:36 +0900

    Update .config/nvim/lua/plugins/diffview.lua

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -1,6 +1,8 @@
 return {
 	"sindrets/diffview.nvim",
 
+	cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewRefresh" },
+
 	opts = {
 		default_args = { -- Default args prepended to the arg-list for the listed commands
 			DiffviewOpen = { "--imply-local" },
