7c4c359af887ba9fa2a8e22ca6309f9fca413259
topazape <topazape@gmail.com>
2026-04-16 10:15:39 +0900

    Update .config/nvim/lua/plugins/diffview.lua

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,6 @@
 return {
 	"sindrets/diffview.nvim",
+	cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
 	opts = {},
 	keys = {
 		{ "<leader>gdc", "<CMD>DiffviewClose<CR>", desc = "Diffview Close" },
