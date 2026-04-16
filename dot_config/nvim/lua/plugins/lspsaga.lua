3385902f7567523642be8cfe8cc69dcbefbf2f46
topazape <topazape@gmail.com>
2026-04-16 16:16:53 +0900

    Update .config/nvim/lua/plugins/lspsaga.lua

--- dot_config/nvim/lua/plugins/lspsaga.lua
+++ dot_config/nvim/lua/plugins/lspsaga.lua
@@ -1,23 +1,26 @@
 return {
 	"nvimdev/lspsaga.nvim",
+
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 	},
-	opts = {
-		-- keymaps for lspsaga
-		vim.keymap.set("n", "K", "<CMD>Lspsaga hover_doc<CR>", { noremap = true }),
-		vim.keymap.set("n", "KK", "<CMD>Lspsaga hover_doc ++keep<CR>", { noremap = true }),
-		vim.keymap.set("n", "gd", "<CMD>Lspsaga peek_definition<CR>", { noremap = true }),
-		vim.keymap.set("n", "gk", "<CMD>Lspsaga peek_type_definition<CR>", { noremap = true }),
-		vim.keymap.set("n", "gr", "<CMD>Lspsaga finder<CR>", { noremap = true }),
-		vim.keymap.set("n", "ga", "<CMD>Lspsaga code_action<CR>", { noremap = true }),
-		vim.keymap.set("n", "gn", "<CMD>Lspsaga rename<CR>", { noremap = true }),
-		vim.keymap.set("n", "ge", "<CMD>Lspsaga show_line_diagnostics<CR>", { noremap = true }),
-		vim.keymap.set("n", "go", "<CMD>Lspsaga outline<CR>", { noremap = true }),
-		vim.keymap.set("n", "g]", "<CMD>Lspsaga diagnostic_jump_next<CR>", { noremap = true }),
-		vim.keymap.set("n", "g[", "<CMD>Lspsaga diagnostic_jump_prev<CR>", { noremap = true }),
-		vim.keymap.set("n", "<C-\\>", "<CMD>Lspsaga term_toggle<CR>", { noremap = true }),
 
+	keys = {
+		{ "K", "<CMD>Lspsaga hover_doc<CR>", noremap = true },
+		{ "KK", "<CMD>Lspsaga hover_doc ++keep<CR>", noremap = true },
+		{ "gd", "<CMD>Lspsaga peek_definition<CR>", noremap = true },
+		{ "gk", "<CMD>Lspsaga peek_type_definition<CR>", noremap = true },
+		{ "gr", "<CMD>Lspsaga finder<CR>", noremap = true },
+		{ "ga", "<CMD>Lspsaga code_action<CR>", noremap = true },
+		{ "gn", "<CMD>Lspsaga rename<CR>", noremap = true },
+		{ "ge", "<CMD>Lspsaga show_line_diagnostics<CR>", noremap = true },
+		{ "go", "<CMD>Lspsaga outline<CR>", noremap = true },
+		{ "g]", "<CMD>Lspsaga diagnostic_jump_next<CR>", noremap = true },
+		{ "g[", "<CMD>Lspsaga diagnostic_jump_prev<CR>", noremap = true },
+		{ "<C-\\>", "<CMD>Lspsaga term_toggle<CR>", noremap = true },
+	},
+
+	opts = {
 		ui = {
 			border = "rounded",
 			code_action = "󱐋 ",
