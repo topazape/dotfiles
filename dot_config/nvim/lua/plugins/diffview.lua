f84fa0f509a3c755de282b0d052fa2fcb74ced89
topazape <topazape@gmail.com>
2026-04-16 16:10:34 +0900

    Add .config/nvim/lua/plugins/diffview.lua

---
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+	"sindrets/diffview.nvim",
+	opts = {
+		default_args = { -- Default args prepended to the arg-list for the listed commands
+			DiffviewOpen = { "--imply-local" },
+			DiffviewFileHistory = {},
+		},
+	},
+}
