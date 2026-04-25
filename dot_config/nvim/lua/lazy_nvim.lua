7705e914edea850a553f54b1c1f5c9c9638c5199
topazape <topazape@gmail.com>
2026-04-25 16:00:06 +0900

    Update .config/nvim/lua/lazy_nvim.lua

--- dot_config/nvim/lua/lazy_nvim.lua
+++ dot_config/nvim/lua/lazy_nvim.lua
@@ -30,9 +30,22 @@ require("lazy").setup({
 		{ import = "plugins.gopher" },
 		{ import = "plugins.lspsaga" },
 		{ import = "plugins.lualine" },
-		{ import = "plugins.render-markdown" },
 		{ import = "plugins.snacks" },
 		{ import = "plugins.treesitter" },
+
+		{
+			"delphinus/md-render.nvim",
+			version = "*",
+			dependencies = {
+				{ "nvim-tree/nvim-web-devicons", version = "*" }, -- optional: file type icons in code blocks
+				{ "delphinus/budoux.lua", version = "*" }, -- optional: CJK phrase-level line breaking
+			},
+			keys = {
+				{ "<leader>mp", "<Plug>(md-render-preview)", desc = "Markdown preview (toggle)" },
+				{ "<leader>mt", "<Plug>(md-render-preview-tab)", desc = "Markdown preview in tab (toggle)" },
+				{ "<leader>md", "<Plug>(md-render-demo)", desc = "Markdown render demo" },
+			},
+		},
 
 		{ "HiPhish/rainbow-delimiters.nvim" },
 		{ "OXY2DEV/helpview.nvim" },
