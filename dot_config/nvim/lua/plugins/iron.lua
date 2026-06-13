b7ba0792ee6564d9d0c754190ff5e9a2699e2ca4
topazape <topazape@gmail.com>
2026-06-13 23:29:00 +0900

    Update .config/nvim/lua/plugins/iron.lua

--- dot_config/nvim/lua/plugins/iron.lua
+++ dot_config/nvim/lua/plugins/iron.lua
@@ -23,14 +23,28 @@ return {
 			},
 
 			keymaps = {
-				toggle_repl = "<space>rr",
-				restart_repl = "<space>rR",
+				toggle_repl = "<space>rr", -- toggles the repl open and closed.
+				-- If repl_open_command is a table as above, then the following keymaps are
+				-- available
+				-- toggle_repl_with_cmd_1 = "<space>rv",
+				-- toggle_repl_with_cmd_2 = "<space>rh",
+				restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
 				send_motion = "<space>sc",
 				visual_send = "<space>sc",
-				send_line = "<space>sl",
 				send_file = "<space>sf",
+				send_line = "<space>sl",
+				send_paragraph = "<space>sp",
+				send_until_cursor = "<space>su",
+				send_mark = "<space>sm",
+				send_code_block = "<space>sb",
+				send_code_block_and_move = "<space>sn",
+				mark_motion = "<space>mc",
+				mark_visual = "<space>mc",
+				remove_mark = "<space>md",
+				cr = "<space>s<cr>",
 				interrupt = "<space>s<space>",
 				exit = "<space>sq",
+				clear = "<space>cl",
 			},
 
 			ignore_blank_lines = true,
