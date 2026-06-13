9cedff28151daea26448e920d8e4aea3896c7cf9
topazape <topazape@gmail.com>
2026-06-13 23:05:47 +0900

    Update .config/nvim/lsp/r-language-server.lua

--- dot_config/nvim/lsp/r-language-server.lua
+++ dot_config/nvim/lsp/r-language-server.lua
@@ -2,4 +2,14 @@ return {
 	cmd = { "R", "--no-echo", "-e", "languageserver::run()" },
 	filetypes = { "r" },
 	root_markers = { "DESCRIPTION", "renv.lock", "rproject.toml" },
+	settings = {
+		r = {
+			lsp = {
+				server_capabilities = {
+					documentFormattingProvider = false,
+					documentRangeFormattingProvider = false,
+				},
+			},
+		},
+	},
 }
