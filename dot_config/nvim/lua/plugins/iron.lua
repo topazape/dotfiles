6c26b28fb443677c14fa413496198db9f71c1270
topazape <topazape@gmail.com>
2026-06-13 22:40:47 +0900

    Update .config/nvim/lua/plugins/iron.lua

--- dot_config/nvim/lua/plugins/iron.lua
+++ dot_config/nvim/lua/plugins/iron.lua
@@ -1,3 +1,37 @@
-{
-  'Vigemus/iron.nvim'
+return {
+	"Vigemus/iron.nvim",
+
+	ft = "r",
+
+	config = function()
+		local iron = require("iron.core")
+		local view = require("iron.view")
+		local common = require("iron.fts.common")
+
+		iron.setup({
+			config = {
+				scratch_repl = true,
+				repl_definition = {
+					r = {
+						command = { "arf" },
+						format = common.bracketed_paste,
+					},
+				},
+				repl_open_cmd = view.bottom(40),
+			},
+
+			keymaps = {
+				toggle_repl = "<space>rr",
+				restart_repl = "<space>rR",
+				send_motion = "<space>sc",
+				visual_send = "<space>sc",
+				send_line = "<space>sl",
+				send_file = "<space>sf",
+				interrupt = "<space>s<space>",
+				exit = "<space>sq",
+			},
+
+			ignore_blank_lines = true,
+		})
+	end,
 }
