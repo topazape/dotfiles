601bbeeae015453cb8b983d9fedbec32274aee52
topazape <topazape@gmail.com>
2026-06-14 10:12:45 +0900

    Update .config/nvim/lua/plugins/iron.lua

--- dot_config/nvim/lua/plugins/iron.lua
+++ dot_config/nvim/lua/plugins/iron.lua
@@ -22,29 +22,33 @@ return {
 				repl_open_cmd = view.split.vertical.botright(80),
 			},
 
+			-- repl 名前空間 <space>r に全集約。高頻度ほど低打鍵。
 			keymaps = {
-				toggle_repl = "<space>rr", -- toggles the repl open and closed.
-				-- If repl_open_command is a table as above, then the following keymaps are
-				-- available
-				-- toggle_repl_with_cmd_1 = "<space>rv",
-				-- toggle_repl_with_cmd_2 = "<space>rh",
-				restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
-				send_motion = "<space>sc",
-				visual_send = "<space>sc",
-				send_file = "<space>sf",
-				send_line = "<space>sl",
-				send_paragraph = "<space>sp",
-				send_until_cursor = "<space>su",
-				send_mark = "<space>sm",
-				send_code_block = "<space>sb",
-				send_code_block_and_move = "<space>sn",
-				mark_motion = "<space>mc",
-				mark_visual = "<space>mc",
-				remove_mark = "<space>md",
-				cr = "<space>s<cr>",
-				interrupt = "<space>s<space>",
-				exit = "<space>sq",
-				clear = "<space>cl",
+				-- REPL 制御
+				toggle_repl = "<space>rr", -- 開閉
+				restart_repl = "<space>rR", -- 再起動
+				interrupt = "<space>rc", -- Ctrl-C 相当 (c=cancel)
+				exit = "<space>rq", -- 終了
+				clear = "<space>rx", -- 画面クリア
+				cr = "<space>r<cr>", -- 改行送信
+
+				-- 送信（高頻度：行・段落・選択・motion）
+				send_line = "<space>rl",
+				send_paragraph = "<space>rp",
+				visual_send = "<space>rs", -- visual: 選択送り
+				send_motion = "<space>rs", -- normal: operator (<space>rs + motion)
+				send_file = "<space>rf",
+				send_until_cursor = "<space>ru",
+
+				-- コードブロック（R チャンク逐次実行）
+				send_code_block = "<space>rb",
+				send_code_block_and_move = "<space>rj", -- j=down 連想
+
+				-- mark 系（低頻度：rm 二段に退避）
+				mark_motion = "<space>rmm",
+				mark_visual = "<space>rmm",
+				send_mark = "<space>rms",
+				remove_mark = "<space>rmd",
 			},
 
 			ignore_blank_lines = true,
