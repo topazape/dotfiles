706ab199cfdd4076222d3c15552129bd8e5b9eee
topazape <topazape@gmail.com>
2026-08-13 13:43:10 +0900

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,10 @@ if status is-interactive
     # start starship
     starship init fish | source
 end
+
+# pnpm
+set -gx PNPM_HOME "/Users/kawai/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
