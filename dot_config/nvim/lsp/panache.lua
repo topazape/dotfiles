a33a4472b1276e31e1848f422e13d160aba9eade
topazape <topazape@gmail.com>
2026-08-15 13:27:35 +0900

    Add .config/nvim/lsp/panache.lua

---
+++ dot_config/nvim/lsp/panache.lua
@@ -0,0 +1,15 @@
+return {
+	cmd = { "panache", "lsp" },
+	filetypes = {
+		"markdown",
+		"quarto",
+		"rmd",
+	},
+	root_markers = {
+		".panache.toml",
+		"panache.toml",
+		"_quarto.yml",
+		"_bookdown.yml",
+		".git",
+	},
+}
