b64c9c043cc305da7942df95d3699cfce60b66dd
topazape <topazape@gmail.com>
2026-09-01 11:01:38 +0900

    Update .config/fish/conf.d/23-env-app.fish

--- dot_config/private_fish/conf.d/23-env-app.fish
+++ dot_config/private_fish/conf.d/23-env-app.fish
@@ -21,3 +21,8 @@ if type -q claude
     set -gx CLAUDE_CONFIG_DIR $XDG_CONFIG_HOME/claude
     set -gx CLAUDE_CODE_EFFORT_LEVEL max
 end
+
+# codex
+if type -q codex
+    set -gx CODEX_HOME $XDG_CONFIG_HOME/codex
+end
